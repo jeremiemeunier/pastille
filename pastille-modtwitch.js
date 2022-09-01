f7c49b3486c7dfffe405f7e657f98e055dfd83fb
Bichon Codeurs <jeremie.meunier74@gmail.com>
2022-09-02 00:20:45 +0200

    Update pastille-modtwitch.js

--- pastille-modtwitch.js
+++ pastille-modtwitch.js
@@ -106,7 +106,7 @@ function fc_isOnLive(data, settings) {
                                                                 .setURL(`https://twitch.tv/${data.twitch.name.toString()}`)
                                                         );
                             let live_txt = `Hey <@&${config_settings.role.announce.toString()}> ! **${data.twitch.name.toString()}** est actuellement en live.`;
-							if(notif_line != undefined) { live_txt += data.notif_line.toString(); }
+							if(data.notif_line != undefined) { live_txt += data.notif_line.toString(); }
 							live_txt += `Il stream : **${_API_data_response.title}** sur **${_API_data_response.game_name}**`
                             settings.announce.send({ content: live_txt, components: [live_button] });
                         }
