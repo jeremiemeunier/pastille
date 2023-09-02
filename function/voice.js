734bfe0596760a692ba885e185131da98ca87e26
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-09-03 01:36:38 +0200

    Update voice.js

--- function/voice.js
+++ function/voice.js
@@ -1,7 +1,8 @@
 const { ChannelType, EmbedBuilder } = require('discord.js');
-const { logger } = require('./base.js');
+const { logsEmiter } = require('../function/logs');
+const { options } = require('../config/settings.json');
 
-const createThreadOnJoin = async (channel, threadChannel, channelConsole, user) => {
+const createThreadOnJoin = async (channel, threadChannel, user) => {
     try {
         const thread = await threadChannel.threads.create({
             name: `Voice : ${channel.name}`,
@@ -11,49 +12,49 @@ const createThreadOnJoin = async (channel, threadChannel, channelConsole, user) 
         });
         await thread.members.add(user);
         const embed = new EmbedBuilder()
-                            .setColor('#20A68E')
+                            .setColor(options.color)
                             .setDescription(`<@${user}> tu as rejoint un salon vocal 🎙️`);
         const message = await thread.send({ embeds: [embed, embedExplicative] });
     }
-    catch(error) { logger(channelConsole, `An error occured\r\n ${error}`); }
+    catch(error) { logsEmiter(`An error occured\r\n ${error}`); }
 }
 
-const joinThreadOnJoin = async (channel, threadChannel, channelConsole, user) => {
+const joinThreadOnJoin = async (channel, threadChannel, user) => {
     try {
         const thread = threadChannel.threads.cache.find(thread => thread.name === `Voice : ${channel.name}`);
         await thread.members.add(user);
         const embed = new EmbedBuilder()
-                            .setColor('#20A68E')
+                            .setColor(options.color)
                             .setDescription(`<@${user}> a rejoint le salon vocal 🎙️`);
         const message = await thread.send({ embeds: [embed] });
     }
-    catch(error) { logger(channelConsole, `An error occured\r\n ${error}`); }
+    catch(error) { logsEmiter(`An error occured\r\n ${error}`); }
 }
 
-const leaveThreadOnLeave = async (channel, threadChannel, channelConsole, user) => {
+const leaveThreadOnLeave = async (channel, threadChannel, user) => {
     try {
         const thread = threadChannel.threads.cache.find(thread => thread.name === `Voice : ${channel.name}`);
         await thread.members.remove(user);
         const embed = new EmbedBuilder()
-                            .setColor('#20A68E')
+                            .setColor(options.color)
                             .setDescription(`<@${user}> a quitter ce salon vocal 💨`);
         const message = await thread.send({ embeds: [embed] });
     }
-    catch(error) { logger(channelConsole, `An error occured\r\n ${error}`); }
+    catch(error) { logsEmiter(`An error occured\r\n ${error}`); }
 }
 
-const deleteThreadOnLeave = async (channel, threadChannel, channelConsole) => {
+const deleteThreadOnLeave = async (channel, threadChannel) => {
     try {
         const thread = threadChannel.threads.cache.find(thread => thread.name === `Voice : ${channel.name}`);
         await thread.delete();
     }
-    catch(error) { logger(channelConsole, `An error occured\r\n ${error}`); }
+    catch(error) { logsEmiter(`An error occured\r\n ${error}`); }
 }
 
 const embedExplicative = new EmbedBuilder()
-                                .setColor('#20A68E')
+                                .setColor(options.color)
                                 .setTitle('Channel vocal dédié')
                                 .setDescription(`Ce salon dédié à votre vocal sera supprimé une fois tout le monde partie.
                                                 Toutes les personnes qui quittent le salon vocal sont automatiquement kick de ce salon dédié`);
 
-module.exports = { createThreadOnJoin, joinThreadOnJoin, leaveThreadOnLeave, deleteThreadOnLeave, embedExplicative }
+module.exports = { createThreadOnJoin, joinThreadOnJoin, leaveThreadOnLeave, deleteThreadOnLeave }
