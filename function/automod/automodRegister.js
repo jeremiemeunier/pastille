4e4ba3dbf5cfe4d6d5fcf7df96b5429d163933cb
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-09-06 15:30:11 +0200

    Update automodRegister.js

--- function/automod/automodRegister.js
+++ function/automod/automodRegister.js
@@ -1,29 +1,32 @@
 const fs = require('node:fs');
 const { logsEmiter } = require('../logs');
+const { automodSanction } = require('./automodSanction');
 
-const automodRegister = (user, warn) => {
+const automodRegister = (user, warn, guild) => {
     const userId = user.user.id;
     const execFolder = __dirname;
     const binFolder = execFolder.replace('function/automod', 'bin/automod/');
-    const today = new Date();
+    const today = Date.parse(new Date());
     const newInfraction = { "warn": warn, "time": today };
 
     try {
         let warnList = JSON.parse(fs.readFileSync(`${binFolder}${userId}.txt`, 'utf8', (error, data) => {
-            if(error) { console.log(`No follow file for this user... Creating him`); }
+            if(error) { logsEmiter(`[automod:register] reading file : ${error}`); }
         }));
         warnList.warns.push(newInfraction);
 
         fs.writeFileSync(`${binFolder}${userId}.txt`, JSON.stringify(warnList), (error) => {
-            if(error) { console.log(`No follow file for this user... Creating him`); }
+            if(error) { logsEmiter(`[automod:register] Writting file : ${error}`); }
         });
     }
     catch(error) {
-        logsEmiter(error);
-        fs.appendFileSync(`${binFolder}${userId}.txt`, JSON.stringify({ "warns": [ newInfraction ] }), (error) => {
-            if(error) { console.log(error); }
+        logsEmiter(`[automod:register] No file : ${error}`);
+        fs.appendFileSync(`${binFolder}${userId}.txt`, JSON.stringify({ "warns": [ newInfraction ], "sanctions": [] }), (error) => {
+            if(error) { logsEmiter(`[automod:register] Creating file : ${error}`); }
         });
     }
+
+    automodSanction(user, guild);
 }
 
 module.exports = { automodRegister }
