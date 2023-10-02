b58b0db4f541d2cf7583ef821365815c38933468
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-02 15:16:43 +0200

    Create clearMessages.js

---
+++ commands/admin/clearMessages.js
@@ -0,0 +1,10 @@
+const commands =
+{
+    name: "clearmessages",
+    description: "Supprimer les messages du channel actuel",
+    default_member_permissions: 0
+};
+
+module.exports = {
+    data: commands
+}
