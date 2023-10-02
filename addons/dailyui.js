2c4bd0be266ea0d9140d44e949128466b4694e23
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-02 15:05:09 +0200

    Update dailyui.js

--- addons/dailyui.js
+++ addons/dailyui.js
@@ -1,49 +1,56 @@
 const fs = require('node:fs');
-const { BOT_ID, BOT_TOKEN, BOT_OWNER_ID, GUILD_ID } = require('../config/secret.json');
-const { Client, EmbedBuilder, GatewayIntentBits, Partials } = require('discord.js');
-const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildVoiceStates],
-    partials: [Partials.Message, Partials.Channel, Partials.Reaction],
-});
+const { EmbedBuilder } = require('discord.js');
+const { options } = require('../config/settings.json');
+const { logsEmiter } = require('../function/logs');
+const { GUILD_ID } = require('../config/secret.json');
 
-// Base function for optimal working
+let client;
 
-client.on('ready', () => {
+const addonsLoaded = async (clientItem, addonsParamsItem) => {
+    
+    client = clientItem;
+    const { channel, role } = addonsParamsItem;
+    const dailyUiList = JSON.parse(fs.readFileSync('./config/addons/dailyui.json'));
+
     setInterval(async () => {
         const actualDate = new Date();
-        const dailyUiList = JSON.parse(fs.readFileSync('../data/dailyui.json'));
 
-        if(actualDate.getHours() === 10 && actualDate.getMinutes() === 23) {
-            let dailyUiAvancement = fs.readFileSync('../bin/dailyui.txt', 'utf8', (err, data) => {
+        if(actualDate.getHours() === 9 && actualDate.getMinutes() === 33) {
+            let dailyUiAvancement = fs.readFileSync('./bin/dailyui.txt', 'utf8', (err, data) => {
                 if(err) { console.log(err); }
                 else { return data; }
             });
-            let nextDailyUiAdvancement = fs.readFileSync('../bin/dailyui.txt', 'utf8', (err, data) => {
+            let nextDailyUiAdvancement = fs.readFileSync('./bin/dailyui.txt', 'utf8', (err, data) => {
                 if(err) { console.log(err); }
                 else { return data; }
             });
-                nextDailyUiAdvancement++;
+            nextDailyUiAdvancement++;
 
             try {
                 const guild = client.guilds.cache.find(guild => guild.id === GUILD_ID);
-                const channel = guild.channels.cache.find(channel => channel.name === 'daily-ui');
+                const addonsChannel = guild.channels.cache.find(addonsChannel => addonsChannel.name === channel);
                 const embed = new EmbedBuilder()
-                                    .setColor(options.color)
-                                    .setTitle(`C'est l'heure de ton dailyUi !`)
-                                    .setDescription(`Pour aujourd'hui : **${dailyUiList[dailyUiAvancement].name}**`);
-                const message = await channel.send({ content: `<@&1118500573675782235> c'est l'heure du DailyUi ! N'hésitez pas à partager vos créations dans le fils`, embeds: [embed] });
+                    .setColor(options.color)
+                    .setTitle(`C'est l'heure de ton dailyUi !`)
+                    .setDescription(`Pour aujourd'hui : **${dailyUiList[dailyUiAvancement].name}**`);
+                const message = await addonsChannel.send({
+                    content: `<@&${role}> c'est l'heure du DailyUi ! N'hésitez pas à partager vos créations dans le fils`,
+                    embeds: [embed]
+                });
                 try {
+                    addonsChannel.setTopic(`DailyUi → ${dailyUiList[dailyUiAvancement].name}`);
                     const thread = await message.startThread({
-                        name: `DailyUi : ${dailyUiList[dailyUiAvancement].name}`,
+                        name: `${dailyUiList[dailyUiAvancement].name}`,
                         autoArchiveDuration: 60,
                         reason: 'Need a separate thread for daily dailyui'
                     });
-                    fs.writeFileSync('../bin/dailyui.txt', nextDailyUiAdvancement.toString());
+                    fs.writeFileSync('./bin/dailyui.txt', nextDailyUiAdvancement.toString());
                 }
                 catch(error) { console.log(error); }
             }
             catch(error) { console.log(error); }
         }
     }, 60000);
-});
-client.login(BOT_TOKEN);
+}
+
+module.exports = { addonsLoaded }
