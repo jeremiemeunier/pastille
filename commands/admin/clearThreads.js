29ad8288e0e53744e19c0a022f806639f2f59824
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-02 15:07:48 +0200

    Create clearThreads.js

---
+++ commands/admin/clearThreads.js
@@ -0,0 +1,10 @@
+const commands =
+{
+    name: "clearthreads",
+    description: "Supprimer les threads du channel actuel",
+    default_member_permissions: 0
+};
+
+module.exports = {
+    data: commands
+}
