510a80bd490b170137f061478115b77de9d531dd
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-02 15:16:49 +0200

    Create commandClear.js

---
+++ events/interaction/commandClear.js
@@ -0,0 +1,37 @@
+const { Events } = require('discord.js');
+const { logsEmiter } = require('../../function/logs');
+const { options } = require ('../../config/settings.json');
+
+let client;
+
+const commandClearInit = (clientItem) => {
+
+    client = clientItem;
+
+    client.on(Events.InteractionCreate, async interaction => {
+        if (!interaction.isChatInputCommand()) return;
+        const { commandName } = interaction;
+
+        if(commandName === 'clearthreads') {
+            const interactChannel = client.channels.cache.find(channel => channel.id === interaction.channelId);
+            const threadsMap = interactChannel.threads.cache;
+
+            await threadsMap.map(thread => {
+                thread.delete();
+            });
+
+            interaction.reply('Tout les threads ont été supprimés');
+        }
+        else if(commandName === 'clearmessages') {
+            const interactChannel = client.channels.cache.find(channel => channel.id === interaction.channelId);
+
+            await interactChannel.messages.fetch().then(messages => {
+                messages.map(message => { message.delete(); });
+            });
+
+            interaction.reply('Tout les messages ont été supprimés');
+        }
+    });
+}
+
+module.exports = { commandClearInit };
