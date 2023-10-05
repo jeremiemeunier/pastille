2159f61dc390e5cf013f269ce287e672c644f252
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-05 11:30:33 +0200

    Update automodRegister.js

--- function/automod/automodRegister.js
+++ function/automod/automodRegister.js
@@ -1,32 +1,88 @@
-const fs = require('node:fs');
 const { logsEmiter } = require('../logs');
+const { PORT, BOT_ID } = require('../../config/secret.json');
+const axios = require('axios');
 const { automodSanction } = require('./automodSanction');
 
-const automodRegister = (user, warn, guild) => {
+const automodRegister = async (user, warn, guildItem) => {
     const userId = user.user.id;
-    const execFolder = __dirname;
-    const binFolder = execFolder.replace('function/automod', 'bin/automod/');
-    const today = Date.parse(new Date());
+    const today = new Date();
     const newInfraction = { "warn": warn, "time": today };
 
     try {
-        let warnList = JSON.parse(fs.readFileSync(`${binFolder}${userId}.txt`, 'utf8', (error, data) => {
-            if(error) { logsEmiter(`[automod:register] reading file : ${error}`); }
-        }));
-        warnList.warns.push(newInfraction);
-
-        fs.writeFileSync(`${binFolder}${userId}.txt`, JSON.stringify(warnList), (error) => {
-            if(error) { logsEmiter(`[automod:register] Writting file : ${error}`); }
+        const requestExistModeration = await axios({
+            method: "get",
+            url: "/moderation/user",
+            baseURL: `http://localhost:${PORT}`,
+            params: {
+                user_id: userId
+            },
+            headers: {
+                "pastille_botid": BOT_ID
+            }
         });
+
+        if(requestExistModeration.data.userExist) {
+            const warnsArray = requestExistModeration.data.userData.warns;
+            const sanctionsArray = requestExistModeration.data.userData.sanctions;
+
+            warnsArray.push(newInfraction);
+            const warnsSize = warnsArray.length;
+
+            if(warnsSize === 3 || warnsSize === 6 || warnsSize === 9 ||
+               warnsSize === 12 || warnsSize === 15 || warnsSize === 18) {
+                logsEmiter(`New sanction emitted for ${userId} → ${warnsSize}`);
+                automodSanction(userId, warnsSize, guildItem);
+            }
+
+            try {
+                const updateModeration = await axios({
+                    method: "put",
+                    url: "/moderation",
+                    baseURL: `http://localhost:${PORT}`,
+                    headers: {
+                        "pastille_botid": BOT_ID
+                    },
+                    data: {
+                        user_id: userId,
+                        warns: warnsArray,
+                        sanctions: sanctionsArray
+                    }
+                });
+
+                logsEmiter(`New warn added for ${userId}`);
+            }
+            catch(error) {
+                logsEmiter(error);
+                logsEmiter(error.data.message);
+            }
+        }
+        else {
+            logsEmiter(`User not exist : ${requestExistModeration.data.userExist}`);
+        }
     }
     catch(error) {
-        logsEmiter(`[automod:register] No file : ${error}`);
-        fs.appendFileSync(`${binFolder}${userId}.txt`, JSON.stringify({ "warns": [ newInfraction ], "sanctions": [] }), (error) => {
-            if(error) { logsEmiter(`[automod:register] Creating file : ${error}`); }
-        });
+        // On créer un nouveau register
+        try {
+            const addNewModeration = await axios({
+                method: "post",
+                url: "/moderation",
+                baseURL: `http://localhost:${PORT}`,
+                headers: {
+                    "pastille_botid": BOT_ID
+                },
+                data: {
+                    user_id: userId,
+                    warns: [
+                        newInfraction
+                    ],
+                    sanctions: []
+                }
+            });
+        }
+        catch(error) {
+            logsEmiter(error);
+        }
     }
-
-    automodSanction(user, guild);
 }
 
 module.exports = { automodRegister }
