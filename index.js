d8cf9d5a06186e7a1badc0e10c8cfa873fa9a3ab
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-05 11:40:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const { BOT_TOKEN, MONGODB_URL, PORT } = require('./config/secret.json');
 // ##### API SETUP ##### \\
 
 const express = require("express");
-const RateLimit = require('express-rate-limit');
 const app = express();
+const RateLimit = require('express-rate-limit');
 const cors = require("cors");
-require("dotenv").config();
-const morgan = require("morgan");
 const mongoose = require("mongoose");
 
 const limiter = RateLimit({
@@ -15,10 +13,9 @@ const limiter = RateLimit({
     max: 100,
 })
 
+app.use(limiter);
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"));
-app.use(limiter);
 
 // BDD
 
