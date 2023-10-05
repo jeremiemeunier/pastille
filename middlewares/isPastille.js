87f57e2686851d8528d28df884da9220a9d56126
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-05 10:16:43 +0200

    Create isPastille.js

---
+++ middlewares/isPastille.js
@@ -0,0 +1,14 @@
+const User = require('../model/User');
+const { BOT_ID } = require("../config/secret.json");
+
+const isPastille = async (req, res, next) => {
+
+    if(req.headers.pastille_botid === BOT_ID) {
+        next();
+    }
+    else {
+        res.status(403).json({ message: "Not authorized" });
+    }
+}
+
+module.exports = isPastille;
