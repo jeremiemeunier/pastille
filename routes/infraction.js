86ee41471c73dc398630a881ccd53e392aab0dc9
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-19 10:37:08 +0200

    Update infraction.js

--- routes/infraction.js
+++ routes/infraction.js
@@ -3,37 +3,16 @@ const router = express.Router();
 const Infraction = require("../model/Infraction");
 const isPastille = require("../middlewares/isPastille");
 
-router.put("/infraction", isPastille, async (req, res) => {
-    const { user_id, warns } = req.body;
-
-    if(!user_id || !warns) {
-        return res.status(409).json({ message: "Please complete all input"});
-    }
-
-    const infractionExist = await Infraction.findOne({ user_id: user_id });
-
-    if(!infractionExist) {
-        return res.status(409).json({ message: "This User do not exist !" });
-    }
-
-    if(infractionExist) {
-        const infractionUpdate = await Infraction.updateOne({ user_id: user_id }, {
-            $set: {
-                warns: warns
-            }
-        });
-
-        res.status(201).json({ message: 'Infraction item updated' });
-    }
-});
-
 router.post("/infraction", isPastille, async (req, res) => {
-    const { user_id, warns } = req.body;
+    const { user_id, reason, date } = req.body;
 
     try {
         const newInfraction = new Infraction({
             user_id: user_id,
-            warns: warns
+            warn: {
+                reason: reason,
+                date: date
+            }
         });
         await newInfraction.save();
 
@@ -44,22 +23,10 @@ router.post("/infraction", isPastille, async (req, res) => {
     }
 });
 
-router.get("/infraction/user", isPastille, async (req, res) => {
+router.get("/infraction/all", isPastille, async (req, res) => {
     const { user_id } = req.query;
-    
-    try {
-        const infractionExist = await Infraction.findOne({ user_id: user_id });
 
-        if(!infractionExist) {
-            res.status(404).json({ message: "User not exist", userExist: false });
-        }
-        else {
-            res.status(200).json({ message: "User exist", userExist: true, userData: infractionExist });
-        }
-    }
-    catch(error) {
-        console.log(error);
-    }    
-});
+    res.status(200).json({ message: user_id });
+})
 
 module.exports = router;
