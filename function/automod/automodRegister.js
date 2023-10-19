7683ed97ad993a5b3b5820c68bb750f96e84c2c0
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-19 10:36:59 +0200

    Update automodRegister.js

--- function/automod/automodRegister.js
+++ function/automod/automodRegister.js
@@ -3,83 +3,45 @@ const { PORT, BOT_ID } = require('../../config/secret.json');
 const axios = require('axios');
 const { automodSanction } = require('./automodSanction');
 
-const automodRegister = async (user, warn, guildItem) => {
+const automodRegister = async (user, reason, guildItem) => {
     const userId = user.user.id;
     const today = new Date();
-    const newInfraction = { "warn": warn, "time": today };
 
+    // On créer un nouveau register
     try {
-        const requestExistInfraction = await axios({
-            method: "get",
-            url: "/infraction/user",
+        const addNewInfraction = await axios({
+            method: "post",
+            url: "/infraction",
             baseURL: `http://localhost:${PORT}`,
-            params: {
-                user_id: userId
-            },
             headers: {
                 "pastille_botid": BOT_ID
+            },
+            data: {
+                user_id: userId,
+                reason: reason,
+                date: today
             }
         });
-
-        if(requestExistInfraction.data.userExist) {
-            const warnsArray = requestExistInfraction.data.userData.warns;
-
-            warnsArray.push(newInfraction);
-            const warnsSize = warnsArray.length;
-
-            if(warnsSize === 3 || warnsSize === 6 || warnsSize === 9 ||
-               warnsSize === 12 || warnsSize === 15 || warnsSize === 18) {
-                logsEmiter(`New sanction emitted for ${userId} → ${warnsSize}`);
-                automodSanction(userId, warnsSize, guildItem);
-            }
-
-            try {
-                const updateInfraction = await axios({
-                    method: "put",
-                    url: "/infraction",
-                    baseURL: `http://localhost:${PORT}`,
-                    headers: {
-                        "pastille_botid": BOT_ID
-                    },
-                    data: {
-                        user_id: userId,
-                        warns: warnsArray
-                    }
-                });
-
-                logsEmiter(`New warn added for ${userId}`);
-            }
-            catch(error) {
-                logsEmiter(error);
-                logsEmiter(error.data.message);
-            }
-        }
-        else {
-            logsEmiter(`User not exist : ${requestExistInfraction.data.userExist}`);
-        }
-    }
-    catch(error) {
-        // On créer un nouveau register
+        
+        // On vérifie le nombre de warn
         try {
-            const addNewInfraction = await axios({
-                method: "post",
-                url: "/infraction",
+            const totalWarnUser = await axios({
+                method: "get",
+                url: "/infraction/all",
                 baseURL: `http://localhost:${PORT}`,
                 headers: {
                     "pastille_botid": BOT_ID
                 },
-                data: {
+                params: {
                     user_id: userId,
-                    warns: [
-                        newInfraction
-                    ]
                 }
             });
+
+            logsEmiter(totalWarnUser.data.message);
         }
-        catch(error) {
-            logsEmiter(error);
-        }
+        catch(error) { logsEmiter(error); }
     }
+    catch(error) { logsEmiter(error); }
 }
 
 module.exports = { automodRegister }
