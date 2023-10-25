c264e285328d55d067a2cdfc1cf0939e4c5cf5cc
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-25 13:05:25 +0200

    Update Infraction.js

--- model/Infraction.js
+++ model/Infraction.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const infraction = mongoose.model("Infraction", {
     user_id: String,
+    guild_id: String,
     warn: {
         reason: String,
         date: String
