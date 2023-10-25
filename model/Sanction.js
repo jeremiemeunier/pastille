f52715312b0b5db0671a3152e3bab4d6536d9983
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-10-25 13:05:27 +0200

    Update Sanction.js

--- model/Sanction.js
+++ model/Sanction.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const sanction = mongoose.model("Sanction", {
     user_id: String,
+    guild_id: String,
     sanction: {
         level: String,
         date: String,
