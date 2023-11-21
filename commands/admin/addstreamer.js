3b6cf0be841640b5170be2225a8a0299af244f02
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-11-21 13:33:27 +0100

    Update addstreamer.js

--- commands/admin/addstreamer.js
+++ commands/admin/addstreamer.js
@@ -1,20 +1,57 @@
 const commands =
 {
     name: "twitchping",
-    description: "Permet d'ajouter un streamer au ping de pastille",
+    description: "Toutes les commandes liée aux pings des streamers",
     default_member_permissions: 0,
     options: [
         {
-            "name": "twitch_id",
-            "description": "L'id du streamer",
-            "type": 4,
-            "required": true
+            "name": "add",
+            "description": "Ajouter un streamer aux pings",
+            "type": 1,
+            options: [
+                {
+                    "name": "twitch_id",
+                    "description": "Id twitch du streamer",
+                    "type": 4,
+                    "required": true
+                },
+                {
+                    "name": "twitch_name",
+                    "description": "Le nom du streamer",
+                    "type": 3,
+                    "required": true
+                },
+                {
+                    "name": "discord_id",
+                    "description": "Id discord du streamer",
+                    "type": 4,
+                    "required": false
+                },
+                {
+                    "name": "discord_name",
+                    "description": "Le pseudo discord du streamer",
+                    "type": 3,
+                    "required": false
+                }
+            ]
         },
         {
-            "name": "content",
-            "description": "Le nom du streamer",
-            "type": 3,
-            "required": true
+            "name": "remove",
+            "description": "Supprimer un streamer des pings",
+            "type": 1,
+            "options": [
+                {
+                    "name": "twitch_name",
+                    "description": "Le pseudo twitch du streamer",
+                    "type": 3,
+                    "required": true
+                }
+            ]
+        },
+        {
+            "name": "list",
+            "description": "Liste les streamer qui sont ping",
+            "type": 1
         }
     ]
 };
