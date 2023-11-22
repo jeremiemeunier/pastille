3db5afa2e85c8df6b997ee80a2f7af12a85293ec
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-11-22 21:27:48 +0100

    Create Setting.js

---
+++ model/Setting.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const setting = mongoose.model("Setting", {
+    guild_id: String,
+    options: {
+      bang: String,
+      color: String,
+      reaction: {
+        rule: String,
+        ticket: String,
+        announce: String,
+        warn: String
+      },
+      channels: {
+        console: String,
+        debug: String,
+        announce: String,
+        help: String,
+        voiceText: String,
+        screenshots: String
+      }
+    },
+    moderation: {
+      automod: Boolean,
+      limit: {
+        emoji: Number,
+        mention: Number,
+        link: Number,
+        invite: Number
+      },
+      imune: Array,
+      roles: {
+        muted: String,
+        rule: String
+      },
+      sanctions: {
+        low: {
+          duration: Number,
+          unit: String
+        },
+        medium: {
+          duration: Number,
+          unit: String
+        },
+        hight: {
+          duration: Number,
+          unit: String
+        },
+        critical: {
+          duration: Number,
+          unit: String
+        }
+      }
+    },
+    addons: Array
+});
+
+module.exports = setting;
