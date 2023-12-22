d79720eff08485bdd8647e62cec017a92e0a8981
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-22 15:13:09 +0100

    Update dailyui.js

--- addons/dailyui.js
+++ addons/dailyui.js
@@ -1,7 +1,7 @@
 const { EmbedBuilder } = require('discord.js');
 const { options } = require('../config/settings.json');
-const { logsEmiter } = require('../function/logs');
-const { GUILD_ID, PORT, BOT_ID } = require('../config/secret.json');
+const { logs } = require('../function/logs');
+const { GUILD_ID, BOT_ID } = require('../config/secret.json');
 const axios = require('axios');
 
 let client;
@@ -9,7 +9,7 @@ let client;
 const addonsLoaded = async (clientItem, addonsParamsItem) => {
     
     client = clientItem;
-    const { channel, role, params, name } = addonsParamsItem;
+    const { channel, role, params } = addonsParamsItem;
     const { hours, minutes } = params;
 
     setInterval(async () => {
@@ -22,7 +22,6 @@ const addonsLoaded = async (clientItem, addonsParamsItem) => {
                 const dailyUiChallenge = await axios({
                     method: 'get',
                     url: '/dailyui',
-                    baseURL: `http://localhost:${PORT}`,
                     headers: {
                         "pastille_botid": BOT_ID
                     }
@@ -56,18 +55,17 @@ const addonsLoaded = async (clientItem, addonsParamsItem) => {
                             const dailyUiChallenge = await axios({
                                 method: 'put',
                                 url: '/dailyui',
-                                baseURL: `http://localhost:${PORT}`,
                                 headers: { "pastille_botid": BOT_ID },
                                 params: { id: _id }
                             });
                         }
-                        catch(error) { logsEmiter(`Addons [${name}] : ⚠️  | Update DailyUi Challenge | ${error}`); }
+                        catch(error) { logs("error", "dailyui:update", error); }
                     }
-                    catch(error) { logsEmiter(`Addons [${name}] : ⚠️  | Creating thread and update topic | ${error}`); }
+                    catch(error) { logs("error", "dailyui:topics", error); }
                 }
-                catch(error) { logsEmiter(`Addons [${name}] : ⚠️  | Send embed | ${error}`); }
+                catch(error) { logs("error", "dailyui:embed", error); }
             }
-            catch(error) { logsEmiter(`Addons [${name}] : ⚠️  | Request challenge | ${error}`); }
+            catch(error) { logs("error", "dailyui:request", error); }
         }
     }, 60000);
 }
