999a74447d28598909aa63d06f9eba040a428f9c
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-22 15:35:39 +0100

    Update voice.js

--- function/voice.js
+++ function/voice.js
@@ -1,5 +1,5 @@
 const { ChannelType, EmbedBuilder } = require('discord.js');
-const { logsEmiter } = require('../function/logs');
+const { logs } = require('../function/logs');
 const { options } = require('../config/settings.json');
 
 const createThreadOnJoin = async (channel, threadChannel, user) => {
@@ -12,11 +12,11 @@ const createThreadOnJoin = async (channel, threadChannel, user) => {
         });
         await thread.members.add(user);
         const embed = new EmbedBuilder()
-                            .setColor(options.color)
-                            .setDescription(`<@${user}> tu as rejoint un salon vocal 🎙️`);
+            .setColor(options.color)
+            .setDescription(`<@${user}> tu as rejoint un salon vocal 🎙️`);
         const message = await thread.send({ embeds: [embed, embedExplicative] });
     }
-    catch(error) { logsEmiter(`An error occured\r\n ${error}`); }
+    catch(error) { logs("error", "voice:thread:create", error); }
 }
 
 const joinThreadOnJoin = async (channel, threadChannel, user) => {
@@ -24,11 +24,11 @@ const joinThreadOnJoin = async (channel, threadChannel, user) => {
         const thread = threadChannel.threads.cache.find(thread => thread.name === `Voice : ${channel.name}`);
         await thread.members.add(user);
         const embed = new EmbedBuilder()
-                            .setColor(options.color)
-                            .setDescription(`<@${user}> a rejoint le salon vocal 🎙️`);
+            .setColor(options.color)
+            .setDescription(`<@${user}> a rejoint le salon vocal 🎙️`);
         const message = await thread.send({ embeds: [embed] });
     }
-    catch(error) { logsEmiter(`An error occured\r\n ${error}`); }
+    catch(error) { logs("error", "voice:thread:join", error); }
 }
 
 const leaveThreadOnLeave = async (channel, threadChannel, user) => {
@@ -36,11 +36,11 @@ const leaveThreadOnLeave = async (channel, threadChannel, user) => {
         const thread = threadChannel.threads.cache.find(thread => thread.name === `Voice : ${channel.name}`);
         await thread.members.remove(user);
         const embed = new EmbedBuilder()
-                            .setColor(options.color)
-                            .setDescription(`<@${user}> a quitter ce salon vocal 💨`);
+            .setColor(options.color)
+            .setDescription(`<@${user}> a quitté ce salon vocal 💨`);
         const message = await thread.send({ embeds: [embed] });
     }
-    catch(error) { logsEmiter(`An error occured\r\n ${error}`); }
+    catch(error) { logs("error", "voice:thread:leave", error); }
 }
 
 const deleteThreadOnLeave = async (channel, threadChannel) => {
@@ -48,13 +48,12 @@ const deleteThreadOnLeave = async (channel, threadChannel) => {
         const thread = threadChannel.threads.cache.find(thread => thread.name === `Voice : ${channel.name}`);
         await thread.delete();
     }
-    catch(error) { logsEmiter(`An error occured\r\n ${error}`); }
+    catch(error) { logs("error", "voice:thread:delete", error); }
 }
 
 const embedExplicative = new EmbedBuilder()
-                                .setColor(options.color)
-                                .setTitle('Channel vocal dédié')
-                                .setDescription(`Ce salon dédié à votre vocal sera supprimé une fois tout le monde partie.
-                                                Toutes les personnes qui quittent le salon vocal sont automatiquement kick de ce salon dédié`);
+    .setColor(options.color)
+    .setTitle('Fil vocal dédié')
+    .setDescription(`Ce salon est dédié à votre channel vocal actuel.\r\n - Il sera automatiquement surpprimer une fois que tout le monde aura quitter le channel.\r\n- Chaque personne qui rejoijd  est automatiquement ajoutée au fil.\r\n- Chaque personne qui quitte le channel vocal est retirer du fil automatiquement.`);
 
 module.exports = { createThreadOnJoin, joinThreadOnJoin, leaveThreadOnLeave, deleteThreadOnLeave }
