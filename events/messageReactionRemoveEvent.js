01756a35a7072ec8335d44be22bc89b72e94c59d
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-22 21:59:36 +0100

    Update messageReactionRemoveEvent.js

--- events/messageReactionRemoveEvent.js
+++ events/messageReactionRemoveEvent.js
@@ -1,8 +1,6 @@
-const fs = require('node:fs');
 const { Events } = require('discord.js');
 const { logs } = require('../function/logs');
-
-const roleSettings = JSON.parse(fs.readFileSync('./config/data/role.json'));
+const { removeRole } = require('./interaction/reaction/reactionRole');
 
 const reactionRemoveEventInit = (client) => {
     client.on(Events.MessageReactionRemove, async (reaction, user) => {
@@ -14,28 +12,7 @@ const reactionRemoveEventInit = (client) => {
         }
     
         if(reaction.message.interaction != undefined) {
-            if(reaction.message.interaction.commandName === 'role') {
-                for(let i = 0;i < roleSettings.length;i++) {
-                    if(reaction.emoji.name === roleSettings[i].emoji) {
-                        const guild = client.guilds.cache.find(guild => guild.id === reaction.message.guildId);
-                        const member = guild.members.cache.find(member => member.id === user.id);
-                        const role = guild.roles.cache.find(role => role.id === roleSettings[i].role);
-    
-                        try { await member.roles.remove(role); }
-                        catch(error) { logs("error", "reaction:remove:rule", error); return; }
-                    }
-                }
-            }
-        }
-        else {
-            if(reaction.emoji.name === '🤓') {
-                const guild = client.guilds.cache.find(guild => guild.id === reaction.message.guildId);
-                const member = guild.members.cache.find(member => member.id === user.id);
-                const role = guild.roles.cache.find(role => role.id === '1118500573675782235');
-    
-                try { await member.roles.remove(role); }
-                catch(error) { logs("error", "reaction:remove:dailyui", error); return; }
-            }
+            if(reaction.message.interaction.commandName === 'role') { removeRole(client, reaction, user); }
         }
     });
 }
