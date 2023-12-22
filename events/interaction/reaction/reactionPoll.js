ef2f3282362dca9264079fae23d9341e288d92ec
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-22 21:59:26 +0100

    Create reactionPoll.js

---
+++ events/interaction/reaction/reactionPoll.js
@@ -0,0 +1,20 @@
+
+const pollReactions = async (client, reaction, user) => {
+  const userReactions = reaction.message.reactions.cache.filter(reaction => reaction.users.cache.has(user.id));
+  const botReactThis = reaction.users.cache.find(user => user.bot === true);
+
+  if(botReactThis === undefined) {
+    try { await reaction.users.remove(user); }
+    catch(error) { logs("error", "reaction:poll:remove", error, reaction.guildId); return; }
+  }
+  else {
+    userReactions.map(async (react) => {
+      if(react.emoji.name !== reaction.emoji.name) {
+        try { await react.users.remove(user); }
+        catch(error) { logs("error", "reaction:poll:remove_2", error, reaction.guildId); return; }
+      }
+    });
+  }
+}
+
+module.exports = { pollReactions }
