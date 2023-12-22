24757022ea969cd52e9c88926227c3a66f3b56d9
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-22 15:30:22 +0100

    Update automodVerifer.js

--- function/automod/automodVerifer.js
+++ function/automod/automodVerifer.js
@@ -1,6 +1,6 @@
 const { moderation } = require('../../config/settings.json');
-const { logsEmiter } = require('../logs');
-const { PORT, BOT_ID } = require('../../config/secret.json');
+const { logs } = require('../logs');
+const { BOT_ID } = require('../../config/secret.json');
 const axios = require("axios");
 
 const automodVerifier = async (clientItem) => {
@@ -8,13 +8,12 @@ const automodVerifier = async (clientItem) => {
     const now = Date.parse(new Date());
     const { muted } = moderation.roles;
 
-    logsEmiter('Start sanctions verifications');
+    logs("infos", "automod:verifier", 'Start sanctions verifications');
     
     try {
         const allSanctions = await axios({
             method: "get",
             url: "/sanction",
-            baseURL: `http://localhost:${PORT}`,
             headers: {
                 "pastille_botid": BOT_ID
             }
@@ -43,14 +42,14 @@ const automodVerifier = async (clientItem) => {
                         }, newTimer);
                     }
                 }
-                else { logsEmiter(`User not find [${user_id}]`); }
+                else { logs(`User not find [${user_id}]`); }
             })
         }
-        else { logsEmiter('No sanction in database'); }
+        else { logs('No sanction in database'); }
     }
-    catch(error) { logsEmiter(error); }
+    catch(error) { logs(error); }
 
-    logsEmiter('End sanctions verifications');
+    logs("infos", "automod:verifier", 'End sanctions verifications');
 }
 
 module.exports = { automodVerifier }
