cea896b0b14400198077001c5ba33834b5641286
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-22 17:59:23 +0100

    Create api.js

---
+++ function/api.js
@@ -0,0 +1,37 @@
+const api = () => {
+
+  // ##### API SETUP ##### \\
+  const express = require("express");
+  const app = express();
+  const RateLimit = require('express-rate-limit');
+  const cors = require("cors");
+  const { logs } = require('../function/logs');
+
+  const limiter = RateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  });
+
+  app.use(limiter);
+  app.use(express.json());
+  app.use(cors());
+
+  // API
+  const infractionRoute = require('../routes/infraction');
+  const sanctionRoute = require('../routes/sanction');
+  const dailyuiRoute = require('../routes/dailyui');
+  const twitchpingRoute = require('../routes/twitch');
+  const addonsRoute = require('../routes/addons');
+
+  app.use(infractionRoute);
+  app.use(sanctionRoute);
+  app.use(dailyuiRoute);
+  app.use(twitchpingRoute);
+  app.use(addonsRoute);
+
+  app.get("/", (req, res) => { res.status(200).json({ message: "Bienvenue sur le Backend de Pastille" }); });
+  app.all("*", (req, res) => { res.status(404).json({ message: "This route do not exist" }); });
+  app.listen(3000, () => { logs('start', 'api', `Started on port 3000`); });
+}
+
+module.exports = { api }
