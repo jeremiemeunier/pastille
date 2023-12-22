f0b27916b00f23949bc9c89604a2db79f4d1988a
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-22 15:30:28 +0100

    Update commandsRegister.js

--- function/commandsRegister.js
+++ function/commandsRegister.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const { REST, Routes } = require('discord.js');
 const { BOT_ID, BOT_TOKEN } = require('../config/secret.json');
-const { logsEmiter } = require('../function/logs');
+const { logs } = require('../function/logs');
 
 const commands = [];
 const foldersPath = path.join(__dirname, '../commands');
@@ -20,7 +20,7 @@ for(const folder of commandFolders) {
         if('data' in command) {
             commands.push(command.data);
         } else {
-            logsEmiter(`[WARNING] The command at ${filePath} is missing a required "data" property.`);
+            logs('warning', 'commands:register', `The command at ${filePath} is missing a required "data" property.`);
         }
     }
 }
@@ -30,14 +30,14 @@ const commandRegister = async (GUILD_ID) => {
     const guildName = client.guilds.cache.find(guild => guild.id === GUILD_ID).name;
     (async () => {
         try {
-            await logsEmiter(`Started refreshing ${commands.length} application (/) commands for ${guildName}.`);
+            logs('infos', 'command:register', `Started refreshing ${commands.length} application (/) commands for ${guildName}.`);
             const data = await rest.put(
                 Routes.applicationGuildCommands(BOT_ID, GUILD_ID),
                 { body: commands },
             );
-            logsEmiter(`Successfully reloaded ${data.length} application (/) commands for ${guildName}.`);
+            logs('infos', 'command:register', `Successfully reloaded ${data.length} application (/) commands for ${guildName}.`);
         }
-        catch (error) { console.error(error); }
+        catch (error) { logs('error', 'command:register', error); }
     })();
 }
 
