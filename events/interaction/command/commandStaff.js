93520f30ccd76f81609cf0fb3bac362a363d8cc5
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-23 11:40:39 +0100

    Update commandStaff.js

--- events/interaction/command/commandStaff.js
+++ events/interaction/command/commandStaff.js
@@ -1,29 +1,33 @@
-const { Events, EmbedBuilder } = require('discord.js');
+const { Events, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { logs } = require('../../../function/logs');
 const { options } = require ('../../../config/settings.json');
 
-let client;
-
-const commandStaffInit = (clientItem) => {
-
-    client = clientItem;
-
-    client.on(Events.InteractionCreate, async interaction => {
-        if (!interaction.isChatInputCommand()) return;
-            const { commandName } = interaction;
-        
-        if(commandName === 'staff') {
-            try {
-                const embed = new EmbedBuilder()
-                    .setColor(`${options.color}`)
-                    .setTitle(`Demande de support`)
-                    .setDescription(`Comment pouvons-nous t'aider ? Si tu as des questions ou des demandes clique sur ${options.reaction.ticket} pour contacter le staff`);
-                const message = await interaction.reply({ embeds: [embed], fetchReply: true });
-                message.react(options.reaction.ticket);
-            }
-            catch(error) { logs("error", "command:staff", error); }
-        }
-    });
+const commandStaffInit = (client) => {
+  client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    const { commandName } = interaction;
+    
+    if(commandName === 'staff') {
+      try {
+        const requestStaffButton = new ActionRowBuilder().addComponents(
+          new ButtonBuilder({
+            label: "Contacter le staff",
+            style: ButtonStyle.Primary,
+            custom_id: "requestStaff"
+          })
+        );
+        const embed = new EmbedBuilder()
+          .setColor(`${options.color}`)
+          .setTitle(`Demande de support`)
+          .setDescription(`Comment pouvons-nous t'aider ? Si tu as des questions ou des demandes clique sur le bouton pour contacter le staff`);
+        const message = await interaction.reply({
+          embeds: [embed],
+          components: [requestStaffButton],
+          fetchReply: false });
+      }
+      catch(error) { logs("error", "command:staff", error, interaction.guildId); }
+    }
+  });
 }
 
 module.exports = { commandStaffInit }
