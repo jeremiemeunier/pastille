91dbf179b867f926517eb4a6559db61294d64de3
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-23 11:40:36 +0100

    Update voiceEvent.js

--- events/voiceEvent.js
+++ events/voiceEvent.js
@@ -1,72 +1,88 @@
 const { Events } = require('discord.js');
 const { logs } = require('../function/logs');
 const { createVoiceThread, joinVoiceThread, leaveVoiceThread, deleteVoiceThread } = require('../function/voice');
-const { channels } = require ('../config/settings.json');
+const { getParams } = require('../function/base');
+
+const countMembers = async (channel, guild) => {
+  try {
+    const connected = channel.members.map(x => x).length;
+    return connected;
+  }
+  catch(error) { logs("error", "voice:count_members", error, guild.id); }
+}
+
+const getTextualChannel = async (channel, guild) => {
+  const guildParams = await getParams(guild);
+  const { channels } = guildParams.options;
+
+  try {
+    if(channel.parentId !== null) {
+      const textual = guild.channels.cache.find(
+        textual => textual.name === channels.voiceText && textual.parentId === channel.parentId
+      );
+  
+      if(textual === undefined) {
+        const textualGlobal = guild.channels.cache.find(
+          textual => textual.name === channels.voiceText && textual.parentId === null
+        );
+
+        return textualGlobal;
+      }
+
+      return textual;
+    }
+    else {
+      const textual = guild.channels.cache.find(
+        textual => textual.name === channels.voiceText && textual.parentId === null
+      );
+  
+      return textual;
+    }
+  }
+  catch(error) { logs("error", "voice:search_textual", error, guild.id); }
+}
 
 const voiceEventInit = (client) => {
-    client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
-        if(newState.channelId === oldState.channelId) { return; }
-        else {
-            const guild = client.guilds.cache.find(guild => guild.id === oldState.guild.id) ||
-                          client.guilds.cache.find(guild => guild.id === newState.guild.id);
-            let textChannel = guild.channels.cache.find(textChannel => textChannel.name === channels.voiceText);
-            try {
-                const user = oldState.member.user.id || newState.member.user.id;
-    
-                if (newState.channelId === null) {
-                    const voiceChannel = guild.channels.cache.find(voiceChannel => voiceChannel.id === oldState.channelId);
-                    const connected = voiceChannel.members.map(x => x).length;
-    
-                    if(voiceChannel.parentId !== null) {
-                        textChannel = guild.channels.cache.find(
-                            textChannel => textChannel.name === channels.voiceText
-                            && textChannel.parentId === voiceChannel.parentId);
-                    }
-    
-                    if(connected === 0) { deleteVoiceThread(voiceChannel, textChannel); }
-                    else { leaveVoiceThread(voiceChannel, textChannel, user); }
-                }
-                else if (oldState.channelId === null) {
-                    const voiceChannel = guild.channels.cache.find(voiceChannel => voiceChannel.id === newState.channelId);
-                    const connected = voiceChannel.members.map(x => x).length;
-    
-                    if(voiceChannel.parentId !== null) {
-                        textChannel = guild.channels.cache.find(
-                            textChannel => textChannel.name === channels.voiceText
-                            && textChannel.parentId === voiceChannel.parentId);
-                    }
-            
-                    if(connected === 1) {  createVoiceThread(voiceChannel, textChannel, user); }
-                    else { joinVoiceThread(voiceChannel, textChannel, user); }
-                }
-                else {
-                    const oldVoiceChannel = guild.channels.cache.find(oldVoiceChannel => oldVoiceChannel.id === oldState.channelId);
-                    const newVoiceChannel = guild.channels.cache.find(newVoiceChannel => newVoiceChannel.id === newState.channelId);
-                    const oldNbConnected = oldVoiceChannel.members.map(x => x).length;
-                    const newNbConnected = newVoiceChannel.members.map(x => x).length;
-                    let oldTextChannel = textChannel;
-                    let newTextChannel = textChannel;
-    
-                    if(oldVoiceChannel.parentId !== null) {
-                        oldTextChannel = guild.channels.cache.find(
-                            oldTextChannel => oldTextChannel.name === channels.voiceText
-                            && oldTextChannel.parentId === oldVoiceChannel.parentId);
-                    }
-                    if(newVoiceChannel.parentId !== null) {
-                        newTextChannel = guild.channels.cache.find(
-                            newTextChannel => newTextChannel.name === channels.voiceText
-                            && newTextChannel.parentId === newVoiceChannel.parentId);
-                    }
-            
-                    if(oldNbConnected === 0) { deleteVoiceThread(oldVoiceChannel, oldTextChannel); }
-                    else { leaveVoiceThread(oldVoiceChannel, oldTextChannel, user); }
-                    if(newNbConnected === 1) { createVoiceThread(newVoiceChannel, newTextChannel, user); }
-                    else { joinVoiceThread(newVoiceChannel, newTextChannel, user); }
-                }
-            }
-            catch(error) { logs('error', 'voice', error); return; }
-        }
-    });
+  client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
+    if(newState.channelId === oldState.channelId) { return; }
+
+    const guild = client.guilds.cache.find(guild => guild.id === oldState.guild.id) ||
+                  client.guilds.cache.find(guild => guild.id === newState.guild.id);
+    try {
+      const user = oldState.member.user.id || newState.member.user.id;
+
+      if (newState.channelId === null) {
+        const voiceChannel = guild.channels.cache.find(voiceChannel => voiceChannel.id === oldState.channelId);
+        const connected = await countMembers(voiceChannel, guild);
+        const textual = await getTextualChannel(voiceChannel, guild);
+
+        if(connected === 0) { deleteVoiceThread(voiceChannel, textual); }
+        else { leaveVoiceThread(voiceChannel, textual, user); }
+      }
+      else if (oldState.channelId === null) {
+        const voiceChannel = guild.channels.cache.find(voiceChannel => voiceChannel.id === newState.channelId);
+        const connected = await countMembers(voiceChannel, guild);
+        const textual = await getTextualChannel(voiceChannel, guild);
+
+        if(connected === 1) {  createVoiceThread(voiceChannel, textual, user); }
+        else { joinVoiceThread(voiceChannel, textual, user); }
+      }
+      else {
+        const oldVoiceChannel = guild.channels.cache.find(oldVoiceChannel => oldVoiceChannel.id === oldState.channelId);
+        const newVoiceChannel = guild.channels.cache.find(newVoiceChannel => newVoiceChannel.id === newState.channelId);
+        const oldConnected = await countMembers(oldVoiceChannel, guild);
+        const newConnected = await countMembers(newVoiceChannel, guild);
+        const oldTextual = await getTextualChannel(oldVoiceChannel, guild);
+        const newTextual = await getTextualChannel(newVoiceChannel, guild);
+
+        if(oldConnected === 0) { deleteVoiceThread(oldVoiceChannel, oldTextual); }
+        else { leaveVoiceThread(oldVoiceChannel, oldTextual, user); }
+        if(newConnected === 1) { createVoiceThread(newVoiceChannel, newTextual, user); }
+        else { joinVoiceThread(newVoiceChannel, newTextual, user); }
+      }
+    }
+    catch(error) { logs('error', 'voice', error); return; }
+  });
 }
 
 module.exports = { voiceEventInit }
