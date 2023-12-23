c26c5c4149261b4e33672a86a53c606b6699e4fc
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-23 14:00:17 +0100

    Change command /clear

--- events/interaction/command/commandClear.js
+++ events/interaction/command/commandClear.js
@@ -1,44 +1,38 @@
 const { Events } = require('discord.js');
 const { logs } = require('../../../function/logs');
 
-let client;
+const commandClearInit = (client) => {
+  client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+    const { commandName } = interaction;
 
-const commandClearInit = (clientItem) => {
+    if(commandName !== "clear") { return; }
 
-    client = clientItem;
+    if(interaction.options.getSubcommand() === 'thread') {
+      const interactChannel = client.channels.cache.find(channel => channel.id === interaction.channelId);
+      const threadsMap = interactChannel.threads.cache;
 
-    client.on(Events.InteractionCreate, async interaction => {
-        if (!interaction.isChatInputCommand()) return;
-        const { commandName } = interaction;
+      await threadsMap.map(async (thread) => {
+        try { await thread.delete(); }
+        catch(error) { logs("error", "command:clear:threads", error); }
+      });
 
-        if(commandName === 'clearthreads') {
-            const interactChannel = client.channels.cache.find(channel => channel.id === interaction.channelId);
-            const threadsMap = interactChannel.threads.cache;
+      interaction.reply({ content: `Tout les threads ont été supprimés`, ephemeral: true });
+    }
+    
+    if(interaction.options.getSubcommand() === 'messages') {
+      const interactChannel = client.channels.cache.find(channel => channel.id === interaction.channelId);
 
-            threadsMap.map(thread => {
-                try {
-                    thread.delete();
-                }
-                catch(error) { logs("error", "command:clear:threads", error); }
-            });
+      await interactChannel.messages.fetch().then((messages) => {
+        messages.map(async (message) => {
+          try { await message.delete(); }
+          catch(error) { logs("error", "command:clear:messages", error); }
+        });
+      });
 
-            interaction.reply({ content: `Tout les threads ont été supprimés`, ephemeral: true });
-        }
-        else if(commandName === 'clearmessages') {
-            const interactChannel = client.channels.cache.find(channel => channel.id === interaction.channelId);
-
-            interactChannel.messages.fetch().then(messages => {
-                messages.map(message => {
-                    try {
-                        message.delete();
-                    }
-                    catch(error) { logs("error", "command:clear:messages", error); }
-                });
-            });
-
-            interaction.reply({ content: `Tout les messages ont été supprimés`, ephemeral: true });
-        }
-    });
+      interaction.reply({ content: `Tout les messages ont été supprimés`, ephemeral: true });
+    }
+  });
 }
 
 module.exports = { commandClearInit };
