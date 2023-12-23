10828d726c65f82acf6f95e45c71755e5e633c88
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-23 14:00:39 +0100

    Update command.js

--- routes/command.js
+++ routes/command.js
@@ -5,17 +5,59 @@ const isPastille = require("../middlewares/isPastille");
 const { logs } = require('../function/logs');
 
 router.get("/commands", isPastille, async (req, res) => {
-  const { guild } = req.query;
+  const { guild_id } = req.query;
 
   try {
-    const allCommandsRequest = await Command.find({ guild_id: guild });
+    const allCommandsRequest = await Command.find({ guild_id: guild_id });
     
     if(allCommandsRequest.length === 0) { res.status(404).json({ message: "No commands" }); }
     else {
       res.status(200).json({ data: allCommandsRequest });
     }
   }
-  catch(error) { logs("error", "api:commands:get", error); }
+  catch(error) {
+    logs("error", "api:commands:get", error);
+    res.status(400).json({ message: "Something went wrong", error: error });
+  }
+});
+
+router.get("/commands/id", isPastille, async (req, res) => {
+  const { id } = req.query;
+
+  try {
+    const commandRequest = await Command.findById({ _id: id });
+    
+    if(!commandRequest) { res.status(404).json({ message: "No command with this _id" }); }
+    else { res.status(200).json({ data: commandRequest }); }
+  }
+  catch(error) {
+    logs("error", "api:commands:get", error);
+    res.status(400).json({ message: "Something went wrong", error: error });
+  }
+});
+
+router.post('/commands/add', isPastille, async (req, res) => {
+  const { terms, guild_id, response } = req.body;
+
+  if(!terms || !guild_id || !response) {
+    res.status(400).json({ message: "You must provide all inputs" });
+  }
+  else {
+    try {
+      const newCommandsRegister = new Command({
+        guild_id: guild_id,
+        terms: terms,
+        response: response
+      });
+  
+      await newCommandsRegister.save();
+      res.status(200).json({ message: "New command added", data: newCommandsRegister });
+    }
+    catch(error) {
+      logs("error", "api:commands:add", error, guild_id);
+      res.status(400).json({ message: "Something went wrong" });
+    }
+  }
 });
 
 module.exports = router;
