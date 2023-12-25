61cf3790a80913927998d242a9bb496dc103063d
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-25 15:57:27 +0100

    Update poll.js

--- commands/base/poll.js
+++ commands/base/poll.js
@@ -2,36 +2,36 @@ const fs = require('node:fs');
 const alphabetLetters = JSON.parse(fs.readFileSync('config/data/alphabet.json'));
 
 const pollOptions = () => {
-    let options = [
-        {
-            "name": "question",
-            "description": "La question que tu souhaite poser",
-            "type": 3,
-            "required": true
-        }
-    ];
-
-    for(let i = 0;i < 21; i++) {
-        let optionContent = {
-            "name": "choice_" + alphabetLetters[i]['letter'],
-            "description": "Indique un choix. Tu peux mettre un emoji en premier pour changer la réaction du bot.",
-            "type": 3,
-            "required": false
-        }
-        options.push(optionContent);
+  let options = [
+    {
+      "name": "question",
+      "description": "La question que tu souhaite poser",
+      "type": 3,
+      "required": true
     }
+  ];
 
-    return options;
+  for(let i = 0;i < 21; i++) {
+    let optionContent = {
+      "name": "choice_" + alphabetLetters[i]['letter'],
+      "description": "Indique un choix. Tu peux mettre un emoji en premier pour changer la réaction du bot.",
+      "type": 3,
+      "required": false
+    }
+    options.push(optionContent);
+  }
+
+  return options;
 };
 
 const commands =
 {
-    name: "poll",
-    description: "Crée un sondage",
-    options: pollOptions(),
-    default_member_permissions: 0
+  name: "poll",
+  description: "Crée un sondage",
+  options: pollOptions(),
+  default_member_permissions: 0
 };
 
 module.exports = {
-    data: commands
+  data: commands
 }
