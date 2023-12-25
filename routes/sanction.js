2de3710d7191961b298fd024cf352c6a714d8692
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-25 22:21:36 +0100

    Update sanction.js

--- routes/sanction.js
+++ routes/sanction.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const Sanction = require("../model/Sanction");
 const isPastille = require("../middlewares/isPastille");
+const { logs } = require("../function/logs");
 
-router.post("/sanction", isPastille, async (req, res) => {
+router.post("/sanction/add", isPastille, async (req, res) => {
   const { user_id, level, date, end, guild_id } = req.body;
 
   try {
@@ -18,9 +19,9 @@ router.post("/sanction", isPastille, async (req, res) => {
     });
     await newSanction.save();
 
-    res.status(200).json({ message: "New sanction items created" });
+    res.status(200).json({ message: "New sanction items created", data: newSanction });
   }
-  catch(error) { console.log(error); }
+  catch(error) { logs("error", "api:sanction:register:post", error); }
 });
 
 router.get("/sanction", isPastille, async (req, res) => {
@@ -28,7 +29,7 @@ router.get("/sanction", isPastille, async (req, res) => {
     const allSanction = await Sanction.find();
     res.status(200).json({ message: "Sanction find", items: allSanction });
   }
-  catch(error) { console.log(error); }
+  catch(error) { logs("error", "api:sanction:get:all", error); }
 })
 
 module.exports = router;
