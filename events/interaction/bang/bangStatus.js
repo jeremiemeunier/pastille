92c582adbd9b816deb9d0f8985033d47e7d784eb
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-25 12:22:18 +0100

    Update bangStatus.js

--- events/interaction/bang/bangStatus.js
+++ events/interaction/bang/bangStatus.js
@@ -1,7 +1,16 @@
+const { logs } = require("../../../function/logs");
 
+const bangStatus = async (message, guild) => {
+  const user = guild.members.cache.find(user => user.id === message.author.id);
+  const voiceChannelUser = guild.channels.cache.find(channel => channel.id === user.voice.channelId);
+  const statusText = message.content.substring(8);
 
-const bangStatus = (message, guild) => {
-  
+  console.log(voiceChannelUser);
+
+  try {
+    await voiceChannelUser.setTopic(statusText);
+  }
+  catch(error) { logs("error", "voice:text:command:status", error, guild.id); }
 }
 
 module.exports = { bangStatus }
