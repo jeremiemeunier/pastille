ba2752790791e84ca4c136e44aa321e92106c4c7
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-26 14:33:10 +0100

    Update commandRule.js

--- events/interaction/command/commandRule.js
+++ events/interaction/command/commandRule.js
@@ -1,60 +1,56 @@
-const { Events, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { logs } = require('../../../function/logs');
 const { getRules, getParams } = require('../../../function/base');
 
-const commandRuleInit = async (client) => {
-  client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    const { commandName } = interaction;
+const commandRuleInit = async (client, interaction) => {
+  const { commandName } = interaction;
+  if(commandName !== "rule") { return; }
+  
+  const rules = await getRules(interaction.guild);
+  const guildParams = await getParams(interaction.guild);
+  const { options } = guildParams;
+  const channel = interaction.channel;
+
+  try {
+    let rulesField = [];
+    rules.map((item, index) => {
+        rulesField.push({
+            name: item.name,
+            value: item.description,
+            inline: false
+        });
+    });
+
+    const acceptRuleButton = new ActionRowBuilder()
+      .addComponents(
+        new ButtonBuilder({
+          label: "Accepter le réglement",
+          style: ButtonStyle.Primary,
+          custom_id: "acceptedRules"
+        })
+      );
     
-    if(commandName === 'rule') {
-      const rules = await getRules(interaction.guild);
-      const guildParams = await getParams(interaction.guild);
-      const { options } = guildParams;
-      const channel = interaction.channel;
-
-      try {
-        let rulesField = [];
-        rules.map((item, index) => {
-            rulesField.push({
-                name: item.name,
-                value: item.description,
-                inline: false
-            });
-        });
-
-        const acceptRuleButton = new ActionRowBuilder()
-          .addComponents(
-            new ButtonBuilder({
-              label: "Accepter le réglement",
-              style: ButtonStyle.Primary,
-              custom_id: "acceptedRules"
-            })
-          );
-        
-        const rulesEmbed = new EmbedBuilder({
-          color: parseInt(options.color, 16),
-          title: "Règles du serveur",
-          description: "Les règles du serveur sont simples.\r\nEn utilisant ce serveur discord, l'utilisateur accepte d'emblée le réglement.",
-          fields: rulesField
-        });
-        const modosEmbed = new EmbedBuilder({
-          color: parseInt(options.color, 16),
-          title: "Modérations",
-          description: "Les décisions des modérateur et de l'équipe du serveur ne sont pas discutable. Pour accompagner et faciliter le travail de la modération, un automod est présent sur ce discord."
-        });
-        const updateEmbed = new EmbedBuilder({
-          color: parseInt(options.color, 16),
-          description: "Les modérateurs ou les admins du serveur peuvent à tout moment et sans communication supplémentaire faire évoluer le réglement."
-        });
-        const message = await channel.send({
-          embeds: [rulesEmbed, modosEmbed, updateEmbed],
-          components: [acceptRuleButton] });
-        interaction.reply({ content: 'Les règles ont bien été envoyé', ephemeral: true });
-      }
-      catch(error) { logs("error", "rule:embed", error, interaction.guild.id); }
-    }
-  });
+    const rulesEmbed = new EmbedBuilder({
+      color: parseInt(options.color, 16),
+      title: "Règles du serveur",
+      description: "Les règles du serveur sont simples.\r\nEn utilisant ce serveur discord, l'utilisateur accepte d'emblée le réglement.",
+      fields: rulesField
+    });
+    const modosEmbed = new EmbedBuilder({
+      color: parseInt(options.color, 16),
+      title: "Modérations",
+      description: "Les décisions des modérateur et de l'équipe du serveur ne sont pas discutable. Pour accompagner et faciliter le travail de la modération, un automod est présent sur ce discord."
+    });
+    const updateEmbed = new EmbedBuilder({
+      color: parseInt(options.color, 16),
+      description: "Les modérateurs ou les admins du serveur peuvent à tout moment et sans communication supplémentaire faire évoluer le réglement."
+    });
+    const message = await channel.send({
+      embeds: [rulesEmbed, modosEmbed, updateEmbed],
+      components: [acceptRuleButton] });
+    interaction.reply({ content: 'Les règles ont bien été envoyé', ephemeral: true });
+  }
+  catch(error) { logs("error", "rule:embed", error, interaction.guild.id); }
 }
 
 module.exports = { commandRuleInit }
