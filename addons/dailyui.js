f2ff41c232186ecfc166a6e50db50e2964aedba2
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-26 12:23:26 +0100

    Update dailyui.js

--- addons/dailyui.js
+++ addons/dailyui.js
@@ -1,73 +1,55 @@
 const { EmbedBuilder } = require('discord.js');
 const { options } = require('../config/settings.json');
 const { logs } = require('../function/logs');
-const { GUILD_ID, BOT_ID } = require('../config/secret.json');
+const { GUILD_ID } = require('../config/secret.json');
 const axios = require('axios');
+const addonsLoaded = async (client, addonsParamsItem) => {
+  const { channel, role, params } = addonsParamsItem;
+  const { hours, minutes } = params;
 
-let client;
+  setInterval(async () => {
+    const actualDate = new Date();
 
-const addonsLoaded = async (clientItem, addonsParamsItem) => {
-    
-    client = clientItem;
-    const { channel, role, params } = addonsParamsItem;
-    const { hours, minutes } = params;
+    if(
+      actualDate.getHours().toString() === hours && 
+      actualDate.getMinutes().toString() === minutes) {
+      try {
+        const dailyUiChallenge = await axios.get("/dailyui");
+        const { _id, title, description } = dailyUiChallenge.data.data;
+        const guild = client.guilds.cache.find(guild => guild.id === GUILD_ID);
+        const addonsChannel = guild.channels.cache.find(addonsChannel => addonsChannel.name === channel);
 
-    setInterval(async () => {
-        const actualDate = new Date();
+        try {
+          const embed = new EmbedBuilder({
+            color: parseInt(options.color, 16),
+            title: `**${title}**`,
+            description: description
+          });
+          const message = await addonsChannel.send({
+            content: `<@&${role}> c'est l'heure du DailyUi ! N'hésitez pas à partager vos créations dans le fils`,
+            embeds: [embed]
+          });
 
-        if(
-            actualDate.getHours().toString() === hours && 
-            actualDate.getMinutes().toString() === minutes) {
-            try {
-                const dailyUiChallenge = await axios({
-                    method: 'get',
-                    url: '/dailyui',
-                    headers: {
-                        "pastille_botid": BOT_ID
-                    }
-                });
+          // On crée le thread lié au message
+          // On change aussi le topic du channel
+          try {
+            addonsChannel.setTopic(`**DailyUi → ${title}** | ${description}`);
+            const thread = await message.startThread({
+              name: `${title}`,
+              autoArchiveDuration: 60,
+              reason: 'Need a separate thread for daily dailyui'
+            });
 
-                const { _id, title, description } = dailyUiChallenge.data.data;
-                const guild = client.guilds.cache.find(guild => guild.id === GUILD_ID);
-                const addonsChannel = guild.channels.cache.find(addonsChannel => addonsChannel.name === channel);
-
-                try {
-                    const embed = new EmbedBuilder()
-                        .setColor(options.color)
-                        .setTitle(`**${title}**`)
-                        .setDescription(`${description}`);
-                    const message = await addonsChannel.send({
-                        content: `<@&${role}> c'est l'heure du DailyUi ! N'hésitez pas à partager vos créations dans le fils`,
-                        embeds: [embed]
-                    });
-
-                    // On crée le thread lié au message
-                    // On change aussi le topic du channel
-                    try {
-                        addonsChannel.setTopic(`**DailyUi → ${title}** | ${description}`);
-                        const thread = await message.startThread({
-                            name: `${title}`,
-                            autoArchiveDuration: 60,
-                            reason: 'Need a separate thread for daily dailyui'
-                        });
-
-                        try {
-                            const dailyUiChallenge = await axios({
-                                method: 'put',
-                                url: '/dailyui',
-                                headers: { "pastille_botid": BOT_ID },
-                                params: { id: _id }
-                            });
-                        }
-                        catch(error) { logs("error", "dailyui:update", error); }
-                    }
-                    catch(error) { logs("error", "dailyui:topics", error); }
-                }
-                catch(error) { logs("error", "dailyui:embed", error); }
-            }
-            catch(error) { logs("error", "dailyui:request", error); }
+            try { const dailyUiChallenge = await axios.put("/dailyui", {}, { params: { id: _id }}); }
+            catch(error) { logs("error", "dailyui:update", error); }
+          }
+          catch(error) { logs("error", "dailyui:topics", error); }
         }
-    }, 60000);
+        catch(error) { logs("error", "dailyui:embed", error); }
+      }
+      catch(error) { logs("error", "dailyui:request", error); }
+    }
+  }, 60000);
 }
 
 module.exports = { addonsLoaded }
