fa892c5c062d5612f1561366cf51958debe78018
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-26 11:37:58 +0100

    Update automodRegister.js

--- function/automod/automodRegister.js
+++ function/automod/automodRegister.js
@@ -1,5 +1,4 @@
 const { logs } = require('../logs');
-const { BOT_ID } = require('../../config/secret.json');
 const axios = require('axios');
 const { automodSanction } = require('./automodSanction');
 
@@ -9,34 +8,16 @@ const automodRegister = async (user, reason, guild) => {
 
   // On créer un nouveau register
   try {
-    const addNewInfraction = await axios({
-      method: "post",
-      url: "/infraction",
-      headers: {
-        "pastille_botid": BOT_ID
-      },
-      data: {
-        user_id: userId,
-        reason: reason,
-        date: today,
-        guild_id: guild.id
-      }
+    const addNewInfraction = await axios.post("/infraction", {
+      user_id: userId,
+      reason: reason,
+      date: today,
+      guild_id: guild.id
     });
     
     // On vérifie le nombre de warn
     try {
-      const totalWarnUser = await axios({
-        method: "get",
-        url: "/infraction/all",
-        headers: {
-          "pastille_botid": BOT_ID
-        },
-        params: {
-          user_id: userId,
-          guild_id: guild.id
-        }
-      });
-
+      const totalWarnUser = await axios.get("/infraction/all", {params: { user_id: userId, guild_id: guild.id }});
       automodSanction(user, totalWarnUser, guild);
     }
     catch(error) { logs("error", "automod:get:infractions", error, guild.id); }
