c3f60c0f38d4a376a87288176d096f656bebaa1a
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-26 15:33:35 +0100

    Change name of file command

---
+++ commands/admin/twitch.js
@@ -0,0 +1,53 @@
+const commands =
+{
+  name: "twitch",
+  description: "Toutes les commandes liée aux pings des streamers",
+  default_member_permissions: 0,
+  options: [
+    {
+      "name": "add",
+      "description": "Ajouter un streamer aux pings",
+      "type": 1,
+      options: [
+        {
+          "name": "twitch_id",
+          "description": "Id twitch du streamer",
+          "type": 4,
+          "required": true
+        },
+        {
+          "name": "twitch_name",
+          "description": "Le nom du streamer",
+          "type": 3,
+          "required": true
+        },
+        {
+          "name": "message",
+          "description": "Un message dédié",
+          "type": 4,
+          "required": false
+        },
+        {
+          "name": "progress",
+          "description": "Force le ping à tout le monde pour aider le streamer",
+          "type": 3,
+          "required": false
+        }
+      ]
+    },
+    {
+      "name": "remove",
+      "description": "Supprimer un streamer des pings",
+      "type": 1
+    },
+    {
+      "name": "list",
+      "description": "Liste les streamers qui sont ping",
+      "type": 1
+    }
+  ]
+};
+
+module.exports = {
+  data: commands
+}
