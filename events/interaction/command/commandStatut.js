46b080d620bae7320449aac89dac7cd2cb2a7ec1
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-26 14:33:55 +0100

    Update commandStatut.js

--- events/interaction/command/commandStatut.js
+++ events/interaction/command/commandStatut.js
@@ -1,27 +1,19 @@
 const { Events, ActivityType } = require('discord.js');
 const { logs } = require('../../../function/logs');
 
-let client;
+const commandStatutInit = async (client, interaction) => {
+  const { commandName } = interaction;
+  if(commandName !== "status") { return; }
 
-const commandStatutInit = (clientItem) => {
-
-    client = clientItem;
-
-    client.on(Events.InteractionCreate, async interaction => {
-        if (!interaction.isChatInputCommand()) return;
-            const { commandName } = interaction;
-        
-        if(commandName === 'statut') {
-            try {
-                client.user.setPresence({
-                    activities: [{ name: interaction.options.getString('content'),
-                    type: ActivityType.Custom
-                }] });
-                await interaction.reply({ content: `Le nouveau nouveau statut de ${client.user} a été définis`, ephemeral: true });
-            }
-            catch(error) { logs("error", "command:status", error); }
-        }
+  try {
+    client.user.setPresence({
+        activities: [{ name: interaction.options.getString('content'),
+        type: ActivityType.Custom
+      }]
     });
+    await interaction.reply({ content: `Le nouveau nouveau statut de ${client.user} a été définis`, ephemeral: true });
+  }
+  catch(error) { logs("error", "command:status", error); }
 }
 
 module.exports = { commandStatutInit }
