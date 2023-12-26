71c5265500f4987d81be45fd88cf36cbd0f8ac0b
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-26 11:38:01 +0100

    Update automodSanction.js

--- function/automod/automodSanction.js
+++ function/automod/automodSanction.js
@@ -1,63 +1,70 @@
-const { EmbedBuilder } = require('discord.js');
 const { logs } = require('../logs');
-const { PORT, BOT_ID } = require('../../config/secret.json');
 const axios = require("axios");
 const { getParams } = require('../base');
 const { automodApply, automodFinalNotify } = require('./automodVerifer');
 
-const automodSanction = async (user, size, guild) => {
+const automodSanctionEvalute = async (size, guild) => {
   const guildParams = await getParams(guild);
   const { moderation } = guildParams;
   const { sanctions } = moderation;
-  const { count } = size.data;
 
   if(
-    count === 3 ||
-    count === 6 ||
-    count === 9) {
+    size === 3 ||
+    size === 6 ||
+    size === 9) {
     // Sanction temporaire de mute LEVEL: low
-    const { low } = sanctions;
-    const duration = durationInterpreter(low)
+    const duration = durationInterpreter(sanctions.low)
     const startTime = new Date();
     const endTime = new Date(Date.parse(new Date()) + duration);
 
-    automodApply(guild, user, duration);
-    sanctionRegister(user.user.id, 'low', startTime, endTime, guild);
+    return { decision: "low", duration: duration, end: endTime, start: startTime }
   }
   else if(
-    count === 12 ||
-    count === 15 ||
-    count === 18
+    size === 12 ||
+    size === 15 ||
+    size === 18
   ) {
     // Sanction temporaire de mute LEVEL: medium
-    const { medium } = sanctions;
-    const duration = durationInterpreter(medium)
+    const duration = durationInterpreter(sanctions.medium)
     const startTime = new Date();
     const endTime = new Date(Date.parse(new Date()) + duration);
 
-    automodApply(guild, user, duration);
-    sanctionRegister(user.user.id, 'medium', startTime, endTime, guild);
+    return { decision: "medium", duration: duration, end: endTime, start: startTime }
   }
   else if(
-    count === 21 ||
-    count === 24 ||
-    count === 27
+    size === 21 ||
+    size === 24 ||
+    size === 27
   ) {
     // Sanction temporaire de mute LEVEL: hight
-    const { hight } = sanctions;
-    const duration = durationInterpreter(hight)
+    const duration = durationInterpreter(sanctions.hight)
     const startTime = new Date();
     const endTime = new Date(Date.parse(new Date()) + duration);
 
-    automodApply(guild, user, duration);
-    sanctionRegister(user.user.id, 'hight', startTime, endTime, guild);
+    return { decision: "hight", duration: duration, end: endTime, start: startTime }
   }
   else if(
-    count > 27
+    size > 27
   ) {
     // Ban définitifs du serveur
+    return { decision: "ban" }
+  }
+
+  return { decision: "nothing" }
+}
+
+const automodSanction = async (user, size, guild) => {
+  const { count } = size.data;
+  const evaluation = await automodSanctionEvalute(count, guild);
+
+  if(evaluation.decision === "nothing") { return; }
+  if(evaluation.decision === "ban") {
     await automodFinalNotify(guild, user);
     guild.members.ban(user);
+  }
+  else {
+    automodApply(guild, user, evaluation.duration);
+    sanctionRegister(user.user.id, evaluation.decision, evaluation.start, evaluation.end, guild);
   }
 }
 
@@ -71,19 +78,12 @@ const durationInterpreter = (sanctionData) => {
 
 const sanctionRegister = async (userId, level, start, end, guild) => {
   try {
-    const register = await axios({
-      method: "post",
-      url: "/sanction/add",
-      headers: {
-        "pastille_botid": BOT_ID
-      },
-      data: {
-        user_id: userId,
-        guild_id: guild.id,
-        level: level,
-        date: start,
-        end: end,
-      }
+    const register = await axios.post("/sanction/add", {
+      user_id: userId,
+      guild_id: guild.id,
+      level: level,
+      date: start,
+      end: end,
     });
   }
   catch(error) { logs("error", "automod:sanction:register:api", error, guild); }
