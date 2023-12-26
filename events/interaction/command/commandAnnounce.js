bceead7aa8b548faf161941e5f871bb04ff8d440
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-26 14:31:28 +0100

    Update commandAnnounce.js

--- events/interaction/command/commandAnnounce.js
+++ events/interaction/command/commandAnnounce.js
@@ -2,27 +2,23 @@ const { Events, EmbedBuilder } = require('discord.js');
 const { logs } = require('../../../function/logs');
 const { getParams } = require('../../../function/base');
 
-const commandAnnounceInit = async (client) => {
-  client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) { return; }
-    const { commandName } = interaction;
+const commandAnnounceInit = async (client, interaction) => {
+  const { commandName } = interaction;
+  if(commandName !== "announce") { return; }
 
-    const guildParams = await getParams(interaction.guild);
-    const { options } = guildParams;
-    
-    if(commandName === 'announce') {
-      try {
-        const embed = new EmbedBuilder({
-          color: parseInt(options.color, 16),
-          title: interaction.options.getString('title'),
-          description: interaction.options.getString('content'),
-        });
-        const message = await interaction.reply({
-          embeds: [embed], fetchReply: true, content: "📢 **Annonce** ||@everyone||" });
-      }
-      catch(error) { logs("error", "command:announce", error); }
-    }
-  });
+  const guildParams = await getParams(interaction.guild);
+  const { options } = guildParams;
+  
+  try {
+    const embed = new EmbedBuilder({
+      color: parseInt(options.color, 16),
+      title: interaction.options.getString('title'),
+      description: interaction.options.getString('content'),
+    });
+    const message = await interaction.reply({
+      embeds: [embed], fetchReply: true, content: "📢 **Annonce** ||@everyone||" });
+  }
+  catch(error) { logs("error", "command:announce", error); }
 }
 
 module.exports = { commandAnnounceInit }
