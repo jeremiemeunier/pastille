d07a691cb498041a1d005ba18e9d0f3b095873c0
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-26 14:31:16 +0100

    Update buttonAcceptRule.js

--- events/interaction/button/buttonAcceptRule.js
+++ events/interaction/button/buttonAcceptRule.js
@@ -2,29 +2,25 @@ const { Events } = require('discord.js');
 const { logs } = require('../../../function/logs');
 const { getParams } = require('../../../function/base');
 
-const buttonAcceptRuleInit = (client) => {
-  client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isButton()) return;
-    const { customId } = interaction;
+const buttonAcceptRuleInit = async (client, interaction) => {
+  const { customId } = interaction;
+  if(customId !== "acceptedRules") { return; }
 
-    const guildParams = await getParams(interaction.guild);
-    const { moderation } = guildParams;
-    
-    if(customId === 'acceptedRules') {
-        const guild = client.guilds.cache.find(guild => guild.id === interaction.guildId);
-        const member = guild.members.cache.find(member => member.id === interaction.user.id);
-        const role = guild.roles.cache.find(role => role.id === moderation.roles.rule);
+  const guildParams = await getParams(interaction.guild);
+  const { moderation } = guildParams;
+  
+  const guild = client.guilds.cache.find(guild => guild.id === interaction.guildId);
+  const member = guild.members.cache.find(member => member.id === interaction.user.id);
+  const role = guild.roles.cache.find(role => role.id === moderation.roles.rule);
 
-        try {
-            await member.roles.add(role);
-            interaction.reply({ content: 'Tu as bien accepté les règles', ephemeral: true });
-        }
-        catch(error) {
-            interaction.reply({ content: 'Une erreur est survenue', ephemeral: true });
-            logs("error", "event:accept_rule", error); return;
-        }
-    }
-  });
+  try {
+    await member.roles.add(role);
+    interaction.reply({ content: 'Tu as bien accepté les règles', ephemeral: true });
+  }
+  catch(error) {
+    interaction.reply({ content: 'Une erreur est survenue', ephemeral: true });
+    logs("error", "event:accept_rule", error); return;
+  }
 }
 
 module.exports = { buttonAcceptRuleInit }
