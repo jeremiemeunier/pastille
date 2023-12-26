cf72a4c5c1d6e1df843883ac6b20a264ce217919
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-26 01:10:50 +0100

    Update sanction.js

--- routes/sanction.js
+++ routes/sanction.js
@@ -4,6 +4,22 @@ const Sanction = require("../model/Sanction");
 const isPastille = require("../middlewares/isPastille");
 const { logs } = require("../function/logs");
 
+router.put("/sanction/update", isPastille, async (req, res) => {
+  const { id } = req.query;
+
+  try {
+    const updateSanction = await Sanction.findByIdAndUpdate(
+      { _id: id },
+      { checkable: false });
+    
+    res.status(200).json({ data: updateSanction });
+  }
+  catch(error) {
+    res.status(400).json({ error: error, message: "An error occured" });
+    logs("error", "api:sanction:put", error, guild_id);
+  }
+});
+
 router.post("/sanction/add", isPastille, async (req, res) => {
   const { user_id, level, date, end, guild_id } = req.body;
 
@@ -15,7 +31,8 @@ router.post("/sanction/add", isPastille, async (req, res) => {
         level: level,
         date: date,
         ending: end
-      }
+      },
+      checkable: true
     });
     await newSanction.save();
 
@@ -28,7 +45,7 @@ router.get("/sanction", isPastille, async (req, res) => {
   const { guild_id } = req.query;
 
   try {
-    const allSanction = await Sanction.find({ guild_id: guild_id });
+    const allSanction = await Sanction.find({ guild_id: guild_id, checkable: true });
     res.status(200).json({ message: "Sanction find", data: allSanction });
   }
   catch(error) { logs("error", "api:sanction:get:all", error); }
