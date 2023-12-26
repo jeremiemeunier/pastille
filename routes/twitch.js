463caadcba94e7d148573a096e2b5448a5a353a4
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-26 15:16:19 +0100

    Update twitch.js

--- routes/twitch.js
+++ routes/twitch.js
@@ -4,6 +4,42 @@ const Streamer = require("../model/Twitch");
 const isPastille = require("../middlewares/isPastille");
 const { logs } = require('../function/logs');
 
+router.get("/twitch", isPastille, async (req, res) => {
+  const { guild_id } = req.query;
+
+  try {
+    const allTwitchPings = await Streamer.find({ guild_id: guild_id });
+
+    if(allTwitchPings.length > 0) {
+      res.status(200).json({
+        message: "Find all pings",
+        data: allTwitchPings
+      })
+    }
+    else { res.status(404).json({ message: "Empty pings" }); }
+  }
+  catch(error) {
+    res.status(400).json({ message: "An error occured", error: error });
+    logs("error", "api:twitch:get", error, guild_id);
+  }
+});
+
+router.get("/twitch/id", isPastille, async (req, res) => {
+  const { id } = req.query;
+
+  try {
+    const twitchItem = Streamer.findById({ _id: id });
+
+    if(!twitchItem) { res.status(404).json({ message: "Item not found" }); }
+
+    res.status(200).json({ message: "Item found", data: twitchItem });
+  }
+  catch(error) {
+    res.status(400).json({ message: "An error occured", error: error });
+    logs("error", "api:twitch:get:id", error);
+  }
+});
+
 router.post("/twitch/add", isPastille, async (req, res) => {
   const { guild_id, twitch_id, twitch_name, message, progress } = req.body;
 
@@ -40,26 +76,6 @@ router.delete("/twitch/remove", isPastille, async (req, res) => {
   catch(error) {
     res.status(400).json({ message: "An error occured", error: error });
     logs("error", "api:twitch:delete", error);
-  }
-});
-
-router.get("/twitch/list", isPastille, async (req, res) => {
-  const { guild_id } = req.query;
-
-  try {
-    const allTwitchPings = await Streamer.find({ guild_id: guild_id });
-
-    if(allTwitchPings.length > 0) {
-      res.status(200).json({
-        message: "Find all pings",
-        data: allTwitchPings
-      })
-    }
-    else { res.status(404).json({ message: "Empty pings" }); }
-  }
-  catch(error) {
-    res.status(400).json({ message: "An error occured", error: error });
-    logs("error", "api:twitch:get", error, guild_id);
   }
 });
 
