5ce1ccb8f22b2b91d9b8b7245e808e18744cc223
Jérémie Meunier <jeremie.meunier74@gmail.com>
2023-12-28 11:57:12 +0100

    Update Setting.js

--- model/Setting.js
+++ model/Setting.js
@@ -19,6 +19,7 @@ const setting = mongoose.model("Setting", {
       channels: {
         alert: String,
         report: String,
+        automod: String,
       },
       limit: {
         emoji: Number,
