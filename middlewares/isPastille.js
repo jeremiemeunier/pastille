c8124e61eccace4d0f355d614cf305b1566cb434
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-03-10 12:20:58 +0100

    Update isPastille.js

--- middlewares/isPastille.js
+++ middlewares/isPastille.js
@@ -1,7 +1,5 @@
-const { BOT_ID } = require("../config/secret.json");
-
 const isPastille = async (req, res, next) => {
-  if (req.headers.pastille_botid === BOT_ID) {
+  if (req.headers.pastille_botid === process.env.BOT_ID) {
     next();
   } else {
     res.status(403).json({ message: "Not authorized" });
