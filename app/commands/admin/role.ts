8590b5e78b8e45754fddcfac14afad21d275fe9d
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-07-25 12:11:15 +0200

    add commands

---
+++ app/commands/admin/role.ts
@@ -0,0 +1,7 @@
+const commands = {
+  name: "role",
+  description: "Créer un message pour ajouter automatiquement les rôles liées",
+  default_member_permissions: 0,
+};
+
+export const data = commands;
---
+++ app/commands/base/reportUser.ts
@@ -0,0 +1,7 @@
+const commands = {
+  name: "Signaler",
+  default_member_permissions: 68608,
+  type: 2,
+};
+
+export const data = commands;
