87c6cf9e148af6d183593f7895cd6cf8786e801c
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-08-01 22:34:27 +0200

    Update bangExecute.ts

--- app/events/interaction/bang/bangExecute.ts
+++ app/events/interaction/bang/bangExecute.ts
@@ -1,30 +1,29 @@
 import { getCommands } from "@functions/base";
 import logs from "@functions/logs";
 
-const bangExecute = async (
-  message: {
-    content?: any;
-    guildId?: any;
-    channelId?: any;
-    author?: { bot: boolean; globalName: any };
-    attachments?: { size: any };
-    startThread?: (arg0: {
-      name: string;
-      autoArchiveDuration: number;
-      reason: string;
-    }) => any;
-    reply?: any;
-  },
-  guild: { id: any },
-  command: any
-) => {
+const bangExecute = async (message: any, guild: any, command: any) => {
   const commandDetails = await getCommands(guild, command);
-  const { response } = commandDetails;
+  const { response, role_id } = commandDetails;
 
-  try {
-    await message.reply({ content: response });
-  } catch (error: any) {
-    logs("error", "bang:personal_command:send", error, guild.id);
+  if (role_id) {
+    const user = guild.members.cache.find(
+      (u: any) => u.id === message.author.id
+    );
+    const userRoles = user.roles.cache;
+
+    if (userRoles.has(role_id)) {
+      try {
+        await message.reply({ content: response });
+      } catch (error: any) {
+        logs("error", "bang:guild:cmd:send", error, guild.id);
+      }
+    }
+  } else {
+    try {
+      await message.reply({ content: response });
+    } catch (error: any) {
+      logs("error", "bang:guild:cmd:send", error, guild.id);
+    }
   }
 };
 
