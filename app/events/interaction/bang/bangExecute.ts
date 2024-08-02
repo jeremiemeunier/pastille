3c4cd1343c0b1f424daeb7b8f7b4b8279c44d77c
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-08-02 10:00:21 +0200

    Update bangExecute.ts

--- app/events/interaction/bang/bangExecute.ts
+++ app/events/interaction/bang/bangExecute.ts
@@ -3,15 +3,34 @@ import logs from "@functions/logs";
 
 const bangExecute = async (message: any, guild: any, command: any) => {
   const commandDetails = await getCommands(guild, command);
-  const { response, role_id } = commandDetails;
+  const { response, role_id, channel_id } = commandDetails;
 
-  if (role_id) {
+  // check if a channel or/and a role is defined to lock command
+
+  if (role_id || channel_id) {
     const user = guild.members.cache.find(
       (u: any) => u.id === message.author.id
     );
     const userRoles = user.roles.cache;
 
-    if (userRoles.has(role_id)) {
+    const params = {
+      role: userRoles.has(role_id),
+      channel: message.channelId === channel_id,
+    };
+
+    if (role_id && channel_id && params.role && params.channel) {
+      try {
+        await message.reply({ content: response });
+      } catch (error: any) {
+        logs("error", "bang:guild:cmd:send", error, guild.id);
+      }
+    } else if (role_id && params.role) {
+      try {
+        await message.reply({ content: response });
+      } catch (error: any) {
+        logs("error", "bang:guild:cmd:send", error, guild.id);
+      }
+    } else if (channel_id && params.channel) {
       try {
         await message.reply({ content: response });
       } catch (error: any) {
