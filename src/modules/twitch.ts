d8c2474ee48bc565ccdbcac1c903d9f8c4c620e6
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-11-19 15:31:01 +0100

    Update twitch.ts

--- src/modules/twitch.ts
+++ src/modules/twitch.ts
@@ -1,6 +1,8 @@
+import { StreamerTypes } from "@/types/Streamers.types";
 import { TwitchTypes } from "@/types/Twitch.types";
 import { getStreamers } from "@functions/base";
 import Logs from "@libs/Logs";
+import pastilleAxios from "@libs/PastilleAxios";
 import TwitchAxios from "@utils/TwitchAxios";
 import {
   EmbedBuilder,
@@ -13,64 +15,113 @@ const waitingTime = 300000;
 const addonsLoaded = async (guild: any, params: any) => {
   Logs("addons:twitch:start", "start", "Starting twitch addons", guild.id);
 
-  // get stream list
-  // group streamer id
+  try {
+    // get stream list
+    // group streamer id
 
-  const authToken = await requestAuthenticator();
-  const streamerList = await getStreamers({ guild: guild });
-
-  const notificationChannel = guild.channels.cache.find(
-    (channel: any) => channel.id === params.channel
-  );
-  const notificationRole = guild.roles.cache.find(
-    (role: any) => role.id === params.role
-  );
-
-  const pingStreamer = setInterval(async () => {
+    // getting auth token from twitch
     const authToken = await requestAuthenticator();
 
-    if (authToken) {
-      streamerList.map(async (item: TwitchTypes) => {
-        const { twitch } = item;
-        const streamerState = await requestStreamerState(twitch.id, authToken);
+    // getting streamer id list from api
+    try {
+      const streamerList = await pastilleAxios.get("/twitch/streamers");
 
-        if (streamerState !== undefined) {
-          if (startAnalyze(streamerState.started_at)) {
+      const eventSubRegister = streamerList.data.map(
+        (streamer: StreamerTypes) =>
+          new Promise(async (resolve, rejet) => {
             try {
-              const liveButton = new ActionRowBuilder().addComponents(
-                new ButtonBuilder({
-                  label: "Rejoindre sur twitch.tv",
-                  style: ButtonStyle.Link,
-                  url: `https://twitch.tv/${twitch.name.toString()}`,
-                })
+              const req = await TwitchAxios.post(
+                "https://api.twitch.tv/helix/eventsub/subscriptions",
+                {
+                  type: "stream.online",
+                  version: 1,
+                  condition: {
+                    broadcaster_user_id: streamer.id,
+                  },
+                  transport: {
+                    method: "webhook",
+                    callback:
+                      "https://pastille.api.jeremiemeunier.fr/twitch/webhook",
+                    secret: process.env.BOT_SECRET_SIG,
+                  },
+                },
+                {
+                  headers: {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${authToken}`,
+                    "Client-ID": process.env.TWITCH_CLIENT,
+                  },
+                }
               );
-              const liveEmbed = new EmbedBuilder({
-                color: parseInt("6441a5", 16),
-                title: `${twitch.name.toString()} est actuellement en live !`,
-                description: `Il stream : **__${streamerState.title}__** sur **__${streamerState.game_name}__**`,
-              });
 
-              try {
-                await notificationChannel.send({
-                  content: `${twitch.name.toString()} est en live ! ${
-                    item.message ? item.message : ""
-                  } ${notificationRole}`,
-                  embeds: [liveEmbed],
-                  components: [liveButton],
-                });
-              } catch (error: any) {
-                Logs("addons:twitch:send", "error", error, guild.id);
-              }
+              Logs("", null, req.data);
             } catch (error: any) {
-              Logs("addons:twitch:ping", "error", error, guild.id);
+              Logs("addon:twitch", "error", error, "event_subscription");
             }
-          }
-        }
-      });
-    } else {
-      Logs("twitch:auth:global", "error", "Cannot auth to twitch");
+          })
+      );
+    } catch (error: any) {
+      Logs("addon:twitch", "warning", error, "get_streamers");
     }
-  }, waitingTime);
+
+    // const notificationChannel = guild.channels.cache.find(
+    //   (channel: any) => channel.id === params.channel
+    // );
+    // const notificationRole = guild.roles.cache.find(
+    //   (role: any) => role.id === params.role
+    // );
+
+    // const pingStreamer = setInterval(async () => {
+    //   const authToken = await requestAuthenticator();
+
+    //   if (authToken) {
+    //     streamerList.map(async (item: TwitchTypes) => {
+    //       const { twitch } = item;
+    //       const streamerState = await requestStreamerState(
+    //         twitch.id,
+    //         authToken
+    //       );
+
+    //       if (streamerState !== undefined) {
+    //         if (startAnalyze(streamerState.started_at)) {
+    //           try {
+    //             const liveButton = new ActionRowBuilder().addComponents(
+    //               new ButtonBuilder({
+    //                 label: "Rejoindre sur twitch.tv",
+    //                 style: ButtonStyle.Link,
+    //                 url: `https://twitch.tv/${twitch.name.toString()}`,
+    //               })
+    //             );
+    //             const liveEmbed = new EmbedBuilder({
+    //               color: parseInt("6441a5", 16),
+    //               title: `${twitch.name.toString()} est actuellement en live !`,
+    //               description: `Il stream : **__${streamerState.title}__** sur **__${streamerState.game_name}__**`,
+    //             });
+
+    //             try {
+    //               await notificationChannel.send({
+    //                 content: `${twitch.name.toString()} est en live ! ${
+    //                   item.message ? item.message : ""
+    //                 } ${notificationRole}`,
+    //                 embeds: [liveEmbed],
+    //                 components: [liveButton],
+    //               });
+    //             } catch (error: any) {
+    //               Logs("addons:twitch:send", "error", error, guild.id);
+    //             }
+    //           } catch (error: any) {
+    //             Logs("addons:twitch:ping", "error", error, guild.id);
+    //           }
+    //         }
+    //       }
+    //     });
+    //   } else {
+    //     Logs("twitch:auth:global", "error", "Cannot auth to twitch");
+    //   }
+    // }, waitingTime);
+  } catch (error: any) {
+    Logs("addon:twitch", "error", error);
+  }
 };
 
 /**
