94c89cb3613674525c90594f184435664a6c5f84
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-11-19 15:30:57 +0100

    Create Streamers.ts

---
+++ src/models/Streamers.ts
@@ -0,0 +1,13 @@
+import { StreamerTypes } from "@/types/Streamers.types";
+import pkg from "mongoose";
+
+const { Schema, model, models } = pkg;
+
+const schema = new Schema<StreamerTypes>({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  sig: { type: String, required: true },
+  announcer: [],
+});
+
+export default models.Streaming || model("Streaming", schema);
