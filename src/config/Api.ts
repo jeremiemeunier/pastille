c6a9e90ce9a29e134836a460b5a4107dd82205b1
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-11-23 12:09:54 +0100

    Update Api.ts

--- src/config/Api.ts
+++ src/config/Api.ts
@@ -1,31 +1,43 @@
-import { Request, Response } from "express";
-import { isPastille } from "@middlewares/isPastille";
+import { json, NextFunction, raw, Request, Response } from "express";
 import Logs from "@libs/Logs";
 import App from "@libs/App";
 
-import infractionRoute from "@routes/infraction";
-import sanctionRoute from "@routes/sanction";
-import dailyuiRoute from "@routes/dailyui";
-import twitchpingRoute from "@routes/twitch";
-import addonsRoute from "@routes/addons";
-import rulesRoute from "@routes/rules";
-import rolesRoute from "@routes/roles";
-import settingsRoute from "@routes/setting";
-import commandsRoute from "@routes/command";
-import emotesRoute from "@routes/emote";
+import { default as infractionRoute } from "@routes/infraction";
+import { default as sanctionRoute } from "@routes/sanction";
+import { default as dailyuiRoute } from "@routes/dailyui";
+import { default as twitchpingRoute } from "@routes/twitch";
+import { default as addonsRoute } from "@routes/addons";
+import { default as rulesRoute } from "@routes/rules";
+import { default as rolesRoute } from "@routes/roles";
+import { default as settingsRoute } from "@routes/setting";
+import { default as commandsRoute } from "@routes/command";
+import { default as emotesRoute } from "@routes/emote";
+import { default as webhookRoute } from "@routes/webhook";
 
 const Api = () => {
   // API
-  App.use(infractionRoute, isPastille);
-  App.use(sanctionRoute, isPastille);
-  App.use(dailyuiRoute, isPastille);
-  App.use(twitchpingRoute, isPastille);
-  App.use(addonsRoute, isPastille);
-  App.use(rulesRoute, isPastille);
-  App.use(rolesRoute, isPastille);
-  App.use(settingsRoute, isPastille);
-  App.use(commandsRoute, isPastille);
-  App.use(emotesRoute, isPastille);
+
+  App.use((req: Request, _res: Response, next: NextFunction) => {
+    if (req.path.startsWith("/twitch/webhook")) {
+      App.use(raw({ type: "application/json" }));
+    } else {
+      App.use(json());
+    }
+
+    next();
+  });
+
+  App.use(infractionRoute);
+  App.use(sanctionRoute);
+  App.use(dailyuiRoute);
+  App.use(twitchpingRoute);
+  App.use(addonsRoute);
+  App.use(rulesRoute);
+  App.use(rolesRoute);
+  App.use(settingsRoute);
+  App.use(commandsRoute);
+  App.use(emotesRoute);
+  App.use(webhookRoute);
 
   App.get("/", (_req: Request, res: Response) => {
     res.status(200).json({ message: "This is pastille" });
