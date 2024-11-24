067db76ab9b4730b76ad598d72d6be570aa0ac82
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-11-24 11:09:27 +0100

    Update webhook.ts

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -1,21 +1,17 @@
 import { raw, Router } from "express";
 import crypto from "crypto";
 import Logs from "@libs/Logs";
+import Streamers from "@models/Streamers";
 
 const router = Router();
 
 router.post("/twitch/webhook", raw({ type: "*/*" }), async (req, res) => {
   const headers = req.headers;
 
-  const eventsub_messageid = headers["twitch-eventsub-message-id"];
-  const eventsub_message_retry = headers["twitch-eventsub-message-retry"];
-  const eventsub_message_type = headers["twitch-eventsub-message-type"];
-  const eventsub_subscription_type =
-    headers["twitch-eventsub-subscription-type"];
-  const eventsub_message_timestamp =
-    headers["twitch-eventsub-message-timestamp"];
-  const eventsub_message_signature =
-    headers["twitch-eventsub-message-signature"];
+  const message_id = headers["twitch-eventsub-message-id"];
+  const message_type = headers["twitch-eventsub-message-type"];
+  const message_timestamp = headers["twitch-eventsub-message-timestamp"];
+  const message_signature = headers["twitch-eventsub-message-signature"];
   const hmac_prefix = "sha256=";
 
   const CheckingSig = (hmac: any, messageSig: any) => {
@@ -26,34 +22,41 @@ router.post("/twitch/webhook", raw({ type: "*/*" }), async (req, res) => {
     return message_id + message_timestamp + body;
   };
 
-  if (
-    eventsub_message_timestamp &&
-    eventsub_messageid &&
-    eventsub_message_signature
-  ) {
-    const hmacMessage = BuildMessage(
-      req.body,
-      eventsub_messageid,
-      eventsub_message_timestamp
-    );
+  if (message_timestamp && message_id && message_signature) {
+    const hmacMessage = BuildMessage(req.body, message_id, message_timestamp);
     const hmac = `${hmac_prefix}${crypto
       .createHmac("sha256", process.env.BOT_SECRET_SIG as string)
       .update(hmacMessage)
       .digest("hex")}`;
 
-    if (CheckingSig(hmac, eventsub_message_signature)) {
-      Logs("", null, eventsub_message_type as string);
+    if (CheckingSig(hmac, message_signature)) {
       // signature verified
       // get body
       const notif = JSON.parse(req.body);
 
       // handle event
-      if (eventsub_message_type === "webhook_callback_verification") {
-        res.status(200).json({ message: "Valid signature" });
-      } else if (eventsub_message_type === "notification") {
+      if (message_type === "webhook_callback_verification") {
+        res.status(200).set("Content-Type", "text/plain").send(notif.challenge);
+      } else if (message_type === "notification") {
         // handle stream online notifications
         const { type } = notif.subscription;
         const { broadcaster_user_id } = notif.event;
+
+        if (type === "stream.online") {
+          try {
+            // find a streamer with this id
+            // update him to indicate is live and unannounced
+            const req = await Streamers.findOneAndUpdate(
+              { id: broadcaster_user_id },
+              { isLive: true, isAnnounce: false },
+              { new: true }
+            );
+          } catch (error: any) {
+            Logs("webhook", "error", error);
+          }
+        }
+
+        res.status(200).set("Content-Type", "text/plain").send(notif.challenge);
       }
     } else {
       res.status(403).json({ message: "Invalid signature" });
