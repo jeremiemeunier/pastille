b109a72c16b73c4b6f56dfc449377784109f8d65
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-11-27 14:29:09 +0100

    Update Streamers.ts

--- src/models/Streamers.ts
+++ src/models/Streamers.ts
@@ -6,7 +6,6 @@ const { Schema, model, models } = pkg;
 const schema = new Schema<StreamerTypes>({
   id: { type: String, required: true },
   name: { type: String, required: true },
-  sig: { type: String, required: true },
   isLive: { type: Boolean, required: true, default: false },
   isAnnounce: { type: Boolean, required: true, default: false },
   isValid: { type: Boolean, required: true, default: false },
