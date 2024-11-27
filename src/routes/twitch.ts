20010eff81c10a173f104e030180eeb65268844d
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-11-27 14:29:04 +0100

    Update twitch.ts

--- src/routes/twitch.ts
+++ src/routes/twitch.ts
@@ -3,15 +3,16 @@ import { isPastille } from "../middlewares/isPastille";
 import Twitch from "@models/Twitch";
 import Logs from "@libs/Logs";
 import Streamers from "@models/Streamers";
+import { StreamerAnnouncerTypes } from "@/types/Streamers.types";
 
 const router = Router();
 
 router.get(
   "/twitch/streamers",
   isPastille,
-  async (req: Request, res: Response) => {
+  async (_req: Request, res: Response) => {
     try {
-      const streamers = await Streamers.find();
+      const streamers = await Streamers.find({ isValid: false });
 
       if (streamers.length > 0) {
         res.status(200).json(streamers);
@@ -27,7 +28,7 @@ router.get(
   }
 );
 
-router.get("/twitch/live", isPastille, async (req: Request, res: Response) => {
+router.get("/twitch/live", isPastille, async (_req: Request, res: Response) => {
   try {
     const query = await Streamers.find({ isLive: true, isAnnounce: false });
 
@@ -46,82 +47,110 @@ router.get("/twitch/live", isPastille, async (req: Request, res: Response) => {
   }
 });
 
-router.get("/twitch", isPastille, async (req: Request, res: Response) => {
-  const { guild_id } = req.query;
-
-  try {
-    const allTwitchPings = await Twitch.find({ guild_id: { $eq: guild_id } });
-
-    if (allTwitchPings.length > 0) {
-      res.status(200).json({
-        message: "Find all pings",
-        data: allTwitchPings,
-      });
-    } else {
-      res.status(404).json({ message: "Empty pings", http_response: 404 });
-    }
-  } catch (error: any) {
-    res.status(400).json({ message: "An error occured", error: error });
-    Logs("api:twitch:get", "error", error, guild_id as string);
-  }
-});
-
-router.get("/twitch/id", isPastille, async (req: Request, res: Response) => {
-  const { id } = req.query;
-
-  try {
-    const twitchItem = Twitch.findById({ _id: { $eq: id } });
-
-    if (!twitchItem) {
-      res.status(404).json({ message: "Item not found", http_response: 404 });
-    }
-
-    res.status(200).json({ message: "Item found", data: twitchItem });
-  } catch (error: any) {
-    res.status(400).json({ message: "An error occured", error: error });
-    Logs("api:twitch:get:id", "error", error);
-  }
-});
-
-router.post("/twitch/add", isPastille, async (req: Request, res: Response) => {
-  const { guild_id, twitch_id, twitch_name, message, progress } = req.body;
-
-  if (!twitch_id || !twitch_name || !guild_id) {
-    res.status(400).json({ message: "Must provide a id and name for twitch" });
-  } else {
-    try {
-      const newAddPing = new Twitch({
-        guild_id: guild_id,
-        twitch: {
-          id: twitch_id,
-          name: twitch_name,
-        },
-        message: message || null,
-        progress: progress || false,
-      });
-      await newAddPing.save();
-      res
-        .status(200)
-        .json({ message: "Twitch added to the list", data: newAddPing });
-    } catch (error: any) {
-      res.status(400).json({ message: "An error occured", error: error });
-      Logs("api:twitch:add", "error", error, guild_id);
-    }
-  }
-});
-
-router.delete(
-  "/twitch/remove",
+router.post(
+  "/twitch/streamers",
   isPastille,
   async (req: Request, res: Response) => {
-    const { id } = req.query;
+    const {
+      streamer_id,
+      streamer_name,
+      guild_id,
+      channel_id,
+      role_id,
+      message,
+      progress,
+    } = req.body;
 
     try {
-      await Twitch.findByIdAndDelete({ _id: { $eq: id } });
-      res.status(200).json({ message: "Item deleted" });
+      const isRegistred = await Streamers.findOne({ id: streamer_id });
+
+      if (isRegistred) {
+        // we already have this streamer
+        // add new recipient
+        const recipients = isRegistred.announcer;
+        recipients.push({
+          guild_id: guild_id,
+          channel_id: channel_id,
+          role_id: role_id,
+          message: message ? message : "",
+          progress: progress ? progress : false,
+        });
+
+        // updating document
+        const updatedDoc = await Streamers.findOneAndUpdate(
+          {
+            id: streamer_id,
+          },
+          {
+            announcer: recipients,
+          },
+          { new: true }
+        );
+        res.status(201).json(updatedDoc);
+      } else {
+        // add new streamer
+        const newDocs = new Streamers({
+          id: streamer_id,
+          name: streamer_name,
+          isLive: false,
+          isAnnounce: false,
+          isValid: false,
+          announcer: [
+            {
+              guild_id: guild_id,
+              channel_id: channel_id,
+              role_id: role_id,
+              message: message ? message : "",
+              progress: progress ? progress : false,
+            },
+          ],
+        });
+
+        await newDocs.save();
+        res.status(201).json(newDocs);
+      }
     } catch (error: any) {
-      res.status(400).json({ message: "An error occured", error: error });
-      Logs("api:twitch:delete", "error", error);
+      Logs("", "error", error, "post_listener");
+      res.status(500).json({ message: "An error occured on listener adding" });
+    }
+  }
+);
+
+router.delete(
+  "/twitch/streamers",
+  isPastille,
+  async (req: Request, res: Response) => {
+    const { streamer_id, guild_id } = req.body;
+
+    try {
+      const isRegistred = await Streamers.findOne({
+        id: streamer_id,
+        announcer: { $in: [{ guild_id: guild_id }] },
+      });
+
+      if (isRegistred.announcer.length > 1) {
+        // we have many announcer
+        const annoucers = isRegistred.annoucer;
+        const newAnnouncer = annoucers.filter(
+          (item: StreamerAnnouncerTypes) => item.guild_id !== guild_id
+        );
+
+        const updatedDoc = await Streamers.findOneAndUpdate(
+          { id: streamer_id },
+          { announcer: newAnnouncer },
+          { new: true }
+        );
+        res.status(201).json(updatedDoc);
+      } else {
+        // we have only one announcer
+        await Streamers.deleteOne({ id: streamer_id });
+        res
+          .status(200)
+          .json({ message: "Document has been removed successfully" });
+      }
+    } catch (error: any) {
+      Logs("", "error", error, "post_listener");
+      res.status(500).json({ message: "An error occured on listener adding" });
     }
   }
 );
