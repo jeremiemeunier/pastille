570df7172ff891857c4a61ee6a88832dc89a7591
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-30 20:32:57 +0100

    Update messageCreateEvent.ts

--- src/events/messageCreateEvent.ts
+++ src/events/messageCreateEvent.ts
@@ -1,4 +1,4 @@
-import { Events } from "discord.js";
+import { Events, Message } from "discord.js";
 import { bangRule } from "./interaction/bang/bangRule";
 import { bangStatus } from "./interaction/bang/bangStatus";
 import { bangExecute } from "./interaction/bang/bangExecute";
@@ -11,78 +11,75 @@ import {
 import Logs from "@libs/Logs";
 
 export const messageCreateEventInit = (client: any) => {
-  client.on(
-    Events.MessageCreate,
-    async (message: {
-      content: any;
-      guildId: any;
-      channelId: any;
-      author: { bot: boolean; globalName: any };
-      attachments: { size: any };
-      startThread: (arg0: {
-        name: string;
-        autoArchiveDuration: number;
-        reason: string;
-      }) => any;
-    }) => {
-      const content = message.content;
-      const guild = client.guilds.cache.find(
-        (guild: { id: any }) => guild.id === message.guildId
-      );
-      const channel = guild.channels.cache.find(
-        (channel: { id: any }) => channel.id === message.channelId
-      );
+  client.on(Events.MessageCreate, async (message: Message) => {
+    const content = message.content;
+    const guild = client.guilds.cache.find(
+      (guild: { id: any }) => guild.id === message.guildId
+    );
+    const channel = guild.channels.cache.find(
+      (channel: { id: any }) => channel.id === message.channelId
+    );
 
-      const guildParams = await getParams({ guild: guild });
-      const { options } = guildParams;
+    const guildParams = await getParams({ guild: guild });
+    const { options } = guildParams;
 
-      const splitedMsg = content.split(" ");
-      const cmd = splitedMsg.shift().slice(1);
+    const splitedMsg = content.split(" ");
+    const cmd = splitedMsg?.shift()?.slice(1);
 
-      if (message.author.bot === true) {
-        return;
-      }
-      if (content.startsWith(options.bang)) {
-        const guildCommands = await getCommands({ guild: guild });
-
-        if (cmd === "regles") {
-          bangRule(message, guild);
-          return;
-        }
-        if (cmd === "status") {
-          bangStatus(message, guild);
-          return;
-        }
-
-        if (guildCommands) {
-          guildCommands.map(async (item: { _id: any; terms: any }) => {
-            const { _id, terms } = item;
-            if (cmd === terms) {
-              await bangExecute(message, guild, _id);
-            }
-          });
-        }
-      }
-
-      if (channel.name === options.channels.screenshots) {
-        if (message.attachments.size) {
-          try {
-            const title = `${
-              message.author.globalName
-            } (${await dateParser()} ${await hoursParser()})`;
-            const thread = await message.startThread({
-              name: title,
-              autoArchiveDuration: 4320,
-              reason: "New screenshots posted",
-            });
-          } catch (error: any) {
-            Logs("thread:screenshots", "error", error);
-            return;
-          }
-        }
-      }
-
+    if (message.author.bot === true) {
       return;
     }
-  );
+
+    if (content.startsWith(options.bang)) {
+      const guildCommands = await getCommands({ guild: guild });
+
+      if (cmd === "regles") {
+        bangRule(message, guild);
+        return;
+      }
+      if (cmd === "status") {
+        bangStatus(message, guild);
+        return;
+      }
+
+      if (guildCommands) {
+        guildCommands.map(async (item: { _id: any; terms: any }) => {
+          const { _id, terms } = item;
+          if (cmd === terms) {
+            await bangExecute(message, guild, _id);
+          }
+        });
+      }
+    }
+
+    if (channel.name === options.channels.screenshots) {
+      if (message.attachments.size) {
+        try {
+          const title = `${
+            message.author.globalName
+          } (${await dateParser()} ${await hoursParser()})`;
+          const thread = await message.startThread({
+            name: title,
+            autoArchiveDuration: 4320,
+            reason: "New screenshots posted",
+          });
+        } catch (error: any) {
+          Logs("thread:screenshots", "error", error);
+          return;
+        }
+      } else {
+        try {
+          await message.delete();
+          await message.author.send(
+            "Tu es sur un channel dédié aux screenshots. Afin de garder de la clarté dans la lecture, tu peux commenter les posts des autres membres directement dans les fils liés."
+          );
+        } catch (error: any) {
+          Logs("delete:message:screenshots", "error", error);
+          return;
+        }
+      }
+    }
+
+    return;
+  });
 };
