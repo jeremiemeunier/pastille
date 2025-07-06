31d460baa8b89be514f7e049569d8024bd0b7dfb
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-07-06 10:58:44 +0200

    Create RateLimiter.ts

---
+++ src/libs/RateLimiter.ts
@@ -0,0 +1,7 @@
+import rateLimit from "express-rate-limit";
+
+export const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: { message: "Too many requests, please try again later." },
+});
