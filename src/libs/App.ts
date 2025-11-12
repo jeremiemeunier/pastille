3a65eab5ba2e77d296337f1067c5d626ec1ed863
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-11-12 20:41:15 +0100

    Add custom HTTP request logging middleware
    Introduced a middleware using morgan and a custom Logs utility to log HTTP requests. This enhances request visibility and debugging by logging method, status, URL, and response time for each request.

--- src/libs/App.ts
+++ src/libs/App.ts
@@ -1,14 +1,39 @@
-import express, { Application, json } from "express";
+import express, {
+  Application,
+  json,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
 import { ensureCsrfToken } from "@middlewares/csrfProtection";
+import morgan from "morgan";
+import Logs from "./Logs";
 
 const App: Application = express();
 
 App.use(cors());
 App.use(cookieParser());
 App.use(json());
+
+App.use((req: Request, res: Response, next: NextFunction) => {
+  morgan((tokens, req, res): any => {
+    Logs(
+      ["http", "requests"],
+      "req",
+      [
+        tokens.method(req, res)?.padEnd(5, " "),
+        tokens.status(req, res),
+        tokens.url(req, res),
+        "-",
+        tokens["response-time"](req, res),
+        "ms",
+      ].join(" ")
+    );
+  })(req, res, next);
+});
 
 // Ensure CSRF tokens are available
 App.use(ensureCsrfToken);
